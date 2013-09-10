1219e9a2988f288ea7001474b4be8e62d6019abe
Ben Morgan <ben.morgan@warwick.ac.uk>
2013-09-10 15:45:08 +0000

    Implement basic library and applications

---
+++ source/flreconstruct/flreconstruct.cc
@@ -0,0 +1,33 @@
+// flreconstruct - SuperNEMO raw data reconstruction application
+//
+// Copyright (c) 2013 by Ben Morgan <bmorgan.warwick@gmail.com> 
+// Copyright (c) 2013 by The University of Warwick
+//
+// This file is part of Falaise.
+//
+// Falaise is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Falaise is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Falaise.  If not, see <http://www.gnu.org/licenses/>.
+
+// Standard Library
+
+// Third Party
+// - A
+
+// This Project
+#include <falaise/version.h>
+
+int main(int argc, char *argv[]) {
+  
+  return 0;
+}
+
