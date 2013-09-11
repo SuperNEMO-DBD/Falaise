6e7f58d86f5a1a8613132cf2764b4b1b45c1553b
Ben Morgan <ben.morgan@warwick.ac.uk>
2013-09-11 22:13:42 +0000

    Implement skeleton simulation
    As per `flreconstruct`, implement the Falaise simulation application
    `flsimulate` as a simple command line skeleton. Provide mock of
    minimal command line arguments with output formatting and error
    handling.

--- source/flsimulate/flsimulate.cc
+++ source/flsimulate/flsimulate.cc
@@ -1,4 +1,4 @@
-// flsimulate - SuperNEMO detector simulation application
+// flsimulate - SuperNEMO raw data reconstruction application
 //
 // Copyright (c) 2013 by Ben Morgan <bmorgan.warwick@gmail.com> 
 // Copyright (c) 2013 by The University of Warwick
@@ -21,13 +21,145 @@
 // Standard Library
 
 // Third Party
-// - A
+// - Bayeux
+#include <bayeux/version.h>
+#include <bayeux/datatools/logger.h>
+
+// - Boost 
+#include <boost/version.hpp>
+#include <boost/program_options.hpp>
 
 // This Project
 #include <falaise/version.h>
 
-int main(int argc, char *argv[]) {
-  
+//----------------------------------------------------------------------
+// HACKY DEMO IMPLEMENTATION
+//
+namespace {
+namespace bpo = boost::program_options;
+
+//! Print version information ot an output stream
+//! Add detailed information if isVerbose is true
+void FLPrintVersion(std::ostream& os, bool isVerbose) {
+  os << "flsimulate " << falaise::version::get_version() << "\n";
+  if (isVerbose) {
+    os << "\n"
+        << "Copyright (C) 2013 SuperNEMO Collaboration\n\n"
+        << "flsimulate uses the following external libraries:\n"
+        << "* Falaise : " << falaise::version::get_version() << "\n"
+        << "* Bayeux  : " << bayeux::version::get_version() << "\n"
+        << "* Boost   : " << BOOST_VERSION << "\n"
+        << "* Geant4  : " << "10.x (eventually)" 
+        << "\n\n";
+  }
+}
+
+//! Return string containing usage message to be printed when help displayed
+std::string FLSimulateUsageMsg() {
+  static std::string flUsageMsg;
+  if (flUsageMsg.empty()) {
+    std::ostringstream os;
+    FLPrintVersion(os,false);
+    os << "Usage:\n"
+       << "  flsimulate [options]\n"
+       << "Options";
+    flUsageMsg = os.str();
+  }
+  return flUsageMsg;
+}
+
+//! Configure and start simulation
+int FLRunSimulation(size_t events, const std::string& sink, bool isVerbose) {
+  datatools::logger::priority logPriority = isVerbose ? datatools::logger::PRIO_INFORMATION : datatools::logger::PRIO_WARNING;
+
+  DT_LOG_INFORMATION(logPriority,"simulation startup");
+  DT_LOG_INFORMATION(logPriority,"will process " << events << " events");
+
+  if (!sink.empty()) {
+    DT_LOG_INFORMATION(logPriority,"data sink   : " << sink);
+  } else {
+    DT_LOG_INFORMATION(logPriority,"null data sink");
+  }
+
+  // Event loop
+  for (size_t i(0); i < events; ++i) {
+    DT_LOG_INFORMATION(logPriority, "processed event " << i);
+  }
+
+  DT_LOG_INFORMATION(logPriority,"simulation shutdown");
   return 0;
+}
+
+//! Run the command line application logic
+int FLSimulateImpl(int argc, char *argv[]) {
+  // - Create options description
+  bpo::options_description opDesc(FLSimulateUsageMsg());
+  opDesc.add_options()
+      ("help", "print this message")
+      ("version", "print version number")
+      ("verbose,v", "increase verbosity of output")
+      ("number,n", 
+       bpo::value<size_t>()->default_value(1)->value_name("[events]"),
+       "number of events to simulate")
+      ("output-file,o", 
+       bpo::value<std::string>()->required()->value_name("[file]"),
+       "path to output file")
+      ;
+
+  //--------------------------
+  // - Parse command line and handle errors
+  bpo::variables_map vMap; 
+  try {
+    bpo::store(bpo::parse_command_line(argc, argv, opDesc), vMap);
+    bpo::notify(vMap);
+  } catch (const bpo::required_option& e) {
+    // We need to handle help/version even if required_option thrown
+    // We leave these for later processing
+    if (!vMap.count("help") && !vMap.count("version")) {
+      std::cerr << "[OptionsException] " << e.what() << std::endl;
+      return 1;
+    }
+  } catch (const std::exception& e) {
+    std::cerr << "[OptionsException] " << e.what() << std::endl;
+    return 1;
+  }
+
+  //----------------------------
+  // - Process options as needed
+  // Handle help
+  if (vMap.count("help")) {
+    std::cout << opDesc << "\n";
+    return 0;
+  }
+
+  // Check for verbose
+  bool isVerbose(vMap.count("verbose"));
+
+  // Handle version
+  if (vMap.count("version")) {
+    FLPrintVersion(std::cout, isVerbose);
+    return 0;
+  }
+
+  // Handle event count
+  size_t eventsToProcess(vMap["number"].as<size_t>());
+
+  // Handle filenames, if any
+  std::string outFile;
+  if (vMap.count("output-file")) {
+    outFile = vMap["output-file"].as<std::string>();
+  }
+
+  int processingCode = FLRunSimulation(eventsToProcess, outFile, isVerbose);
+  return processingCode;
+}
+} // namespace
+
+//----------------------------------------------------------------------
+// Here's the main
+//
+int main(int argc, char *argv[]) {
+  int ret_code = FLSimulateImpl(argc, argv);
+  return ret_code;
 }
 
