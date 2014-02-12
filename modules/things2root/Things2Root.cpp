89c97cd973076953dd1d838a1f693f16ea348b8e
Ben Morgan <ben.morgan@warwick.ac.uk>
2014-02-12 17:37:29 +0000

    Output calibrated data in Things2Root
    Patch supplied by Yorck Ramachers
    
    Add output of calorimeter and calibrated data banks for all components
    in Things2Root. Update example script to preprocess input data by
    running mock digitization and calibration before performing conversion
    to flat ROOT tree.

--- modules/things2root/Things2Root.cpp
+++ modules/things2root/Things2Root.cpp
@@ -58,6 +58,7 @@ void Things2Root::initialize(const datatools::properties& myConfig,
                 std::runtime_error,
                 "Null pointer to geometry manager return by geometry_service");
   }
+  std::cout << "In INIT: create TFile " << std::endl;
 
   // Next all root file output here
   hfile_ = new TFile(filename_output_.c_str(),"RECREATE","Output file of Simulation data");
@@ -68,7 +69,46 @@ void Things2Root::initialize(const datatools::properties& myConfig,
   // explicitly avoid the tree to be reaffcted to another concurrent TFile
   // (output_module & brio format):
   // TO BE CHECKED
-  /* tree_->SetDirectory(hfile_); */
+  // 2014-02-11, YR, seems to work fine
+  tree_->SetDirectory(hfile_);
+
+  // header data
+  tree_->Branch("header.runnumber",&header_.runnumber_);
+  tree_->Branch("header.eventnumber",&header_.eventnumber_);
+  tree_->Branch("header.date",&header_.date_);
+  tree_->Branch("header.runtype",&header_.runtype_);
+  tree_->Branch("header.simulated",&header_.simulated_);
+
+  // calibrated tracker data
+  tree_->Branch("tracker.nohits",&tracker_.nohits_);
+  tree_->Branch("tracker.id",&tracker_.id_);
+  tree_->Branch("tracker.module",&tracker_.module_);
+  tree_->Branch("tracker.side",&tracker_.side_);
+  tree_->Branch("tracker.layer",&tracker_.layer_);
+  tree_->Branch("tracker.column",&tracker_.column_);
+  tree_->Branch("tracker.x",&tracker_.x_);
+  tree_->Branch("tracker.y",&tracker_.y_);
+  tree_->Branch("tracker.z",&tracker_.z_);
+  tree_->Branch("tracker.sigmaz",&tracker_.sigmaz_);
+  tree_->Branch("tracker.r",&tracker_.r_);
+  tree_->Branch("tracker.sigmar",&tracker_.sigmar_);
+  tree_->Branch("tracker.truehitid",&tracker_.truehitid_);
+
+  // calibrated calorimeter data
+  tree_->Branch("calo.nohits",&calo_.nohits_);
+  tree_->Branch("calo.id",&calo_.id_);
+  tree_->Branch("calo.module",&calo_.module_);
+  tree_->Branch("calo.side",&calo_.side_);
+  tree_->Branch("calo.column",&calo_.column_);
+  tree_->Branch("calo.row",&calo_.row_);
+  tree_->Branch("calo.wall",&calo_.wall_);
+  tree_->Branch("calo.time",&calo_.time_);
+  tree_->Branch("calo.sigmatime",&calo_.sigmatime_);
+  tree_->Branch("calo.energy",&calo_.energy_);
+  tree_->Branch("calo.sigmaenergy",&calo_.sigmaenergy_);
+  tree_->Branch("calo.type",&calo_.type_);
+
+  // truth tracker data
   tree_->Branch("truetracker.nohits",&truetracker_.nohits_);
   tree_->Branch("truetracker.id",&truetracker_.id_);
   tree_->Branch("truetracker.module",&truetracker_.module_);
@@ -85,11 +125,28 @@ void Things2Root::initialize(const datatools::properties& myConfig,
   tree_->Branch("truetracker.trackid",&truetracker_.trackid_);
   tree_->Branch("truetracker.parenttrackid",&truetracker_.parenttrackid_);
 
+  // truth calorimeter data
+  tree_->Branch("truecalo.nohits",&truecalo_.nohits_);
+  tree_->Branch("truecalo.id",&truecalo_.id_);
+  tree_->Branch("truecalo.type",&truecalo_.type_);
+  tree_->Branch("truecalo.x",&truecalo_.x_);
+  tree_->Branch("truecalo.y",&truecalo_.y_);
+  tree_->Branch("truecalo.z",&truecalo_.z_);
+  tree_->Branch("truecalo.time",&truecalo_.time_);
+  tree_->Branch("truecalo.energy",&truecalo_.energy_);
+  tree_->Branch("truecalo.module",&truecalo_.module_);
+  tree_->Branch("truecalo.side",&truecalo_.side_);
+  tree_->Branch("truecalo.wall",&truecalo_.wall_);
+  tree_->Branch("truecalo.column",&truecalo_.column_);
+  tree_->Branch("truecalo.row",&truecalo_.row_);
+
+  // truth vertex data
   tree_->Branch("truevertex.x",&truevertex_.x_);
   tree_->Branch("truevertex.y",&truevertex_.y_);
   tree_->Branch("truevertex.z",&truevertex_.z_);
   tree_->Branch("truevertex.time",&truevertex_.time_);
 
+  // truth primary particle data
   tree_->Branch("trueparticle.noparticles",&trueparticle_.noparticles_);
   tree_->Branch("trueparticle.id",&trueparticle_.id_);
   tree_->Branch("trueparticle.type",&trueparticle_.type_);
@@ -106,6 +163,33 @@ void Things2Root::initialize(const datatools::properties& myConfig,
 dpp::base_module::process_status Things2Root::process(datatools::things& workItem) {
   // Local variables
 
+  // calibrated tracker data
+  std::vector<int> trackerid;
+  std::vector<int> trackermodule;
+  std::vector<int> trackerside;
+  std::vector<int> trackerlayer;
+  std::vector<int> trackercolumn;
+  std::vector<double> trackerx;
+  std::vector<double> trackery;
+  std::vector<double> trackerz;
+  std::vector<double> trackersigmaz;
+  std::vector<double> trackerr;
+  std::vector<double> trackersigmar;
+  std::vector<int> trackertruehitid;
+
+  // calibrated calorimeter data
+  std::vector<int> caloid;
+  std::vector<int> calomodule;
+  std::vector<int> caloside;
+  std::vector<int> calocolumn;
+  std::vector<int> calorow;
+  std::vector<int> calowall;
+  std::vector<double> calotime;
+  std::vector<double> calosigmatime;
+  std::vector<double> caloenergy;
+  std::vector<double> calosigmaenergy;
+  std::vector<int> calotype;
+
   // for true gg hits
   std::vector<int> truetrackerid;
   std::vector<int> truetrackermodule;
@@ -121,6 +205,20 @@ dpp::base_module::process_status Things2Root::process(datatools::things& workIte
   std::vector<double> truetrackerzstop;
   std::vector<int> truetrackertrackid;
   std::vector<int> truetrackerparenttrackid;
+
+  // for true calo hits
+  std::vector<int> truecaloid;
+  std::vector<int> truecalotype;
+  std::vector<int> truecalomodule;
+  std::vector<int> truecaloside;
+  std::vector<int> truecalocolumn;
+  std::vector<int> truecalorow;
+  std::vector<int> truecalowall;
+  std::vector<double> truecalox;
+  std::vector<double> truecaloy;
+  std::vector<double> truecaloz;
+  std::vector<double> truecalotime;
+  std::vector<double> truecaloenergy;
 
   // for primary particles
   std::vector<int> partid;
@@ -166,6 +264,7 @@ dpp::base_module::process_status Things2Root::process(datatools::things& workIte
       trueparticle_.time_ = &parttime;
       trueparticle_.ke_ = &partenergy;
 
+      // tracker truth hits
       if (SD.has_step_hits("gg")) {
         int nggtruehits = SD.get_number_of_step_hits("gg");
         truetracker_.nohits_ = nggtruehits;
@@ -215,8 +314,227 @@ dpp::base_module::process_status Things2Root::process(datatools::things& workIte
       truetracker_.trackid_ = &truetrackertrackid;
       truetracker_.parenttrackid_ = &truetrackerparenttrackid;
 
-      tree_->Fill();
+      // calorimeter truth hits
+      truecalo_.nohits_ = 0;
+
+      if (SD.has_step_hits ("calorimeter"))
+	{
+	  truecalo_.nohits_ += SD.get_number_of_step_hits ("calorimeter");
+	  for (int ihit = 0; ihit < truecalo_.nohits_; ihit++)
+	    {
+	      const mctools::base_step_hit& the_scin_hit= SD.get_step_hit ("calorimeter", ihit);
+
+	      static int gid_calo_module_index
+		= geometry_manager_->get_id_mgr ().get_category_info ("calorimeter_block").get_subaddress_index ("module");
+	      static int gid_calo_side_index
+		= geometry_manager_->get_id_mgr ().get_category_info ("calorimeter_block").get_subaddress_index ("side");
+	      static int gid_calo_column_index
+		= geometry_manager_->get_id_mgr ().get_category_info ("calorimeter_block").get_subaddress_index ("column");
+	      static int gid_calo_row_index
+		= geometry_manager_->get_id_mgr ().get_category_info ("calorimeter_block").get_subaddress_index ("row");
+
+	      truecaloid.push_back(the_scin_hit.get_hit_id());
+	      truecalox.push_back(the_scin_hit.get_position_start ().x () / CLHEP::cm);
+	      truecaloy.push_back(the_scin_hit.get_position_start ().y () / CLHEP::cm);
+	      truecaloz.push_back(the_scin_hit.get_position_start ().z () / CLHEP::cm);
+	      truecalotime.push_back(the_scin_hit.get_time_start ()     / CLHEP::ns);
+	      truecaloenergy.push_back(the_scin_hit.get_energy_deposit () / CLHEP::MeV);
+
+	      truecalotype.push_back(0);
+	      truecalowall.push_back(0);
+	      truecalomodule.push_back(the_scin_hit.get_geom_id ().get (gid_calo_module_index));
+	      truecaloside.push_back(the_scin_hit.get_geom_id ().get (gid_calo_side_index));
+	      truecalocolumn.push_back(the_scin_hit.get_geom_id ().get (gid_calo_column_index));
+	      truecalorow.push_back(the_scin_hit.get_geom_id ().get (gid_calo_row_index));
+	    }
+	}
+
+      if (SD.has_step_hits ("xcalo"))
+	{
+	  truecalo_.nohits_ += SD.get_number_of_step_hits ("xcalo");
+	  for (unsigned int ihit = 0; ihit < SD.get_number_of_step_hits ("xcalo"); ihit++)
+	    {
+	      const mctools::base_step_hit& the_scin_hit= SD.get_step_hit ("xcalo", ihit);
+
+	      static int gid_xcalo_module_index
+		= geometry_manager_->get_id_mgr ().get_category_info ("xcalo_block").get_subaddress_index ("module");
+	      static int gid_xcalo_side_index
+		= geometry_manager_->get_id_mgr ().get_category_info ("xcalo_block").get_subaddress_index ("side");
+	      static int gid_xcalo_wall_index
+		= geometry_manager_->get_id_mgr ().get_category_info ("xcalo_block").get_subaddress_index ("wall");
+	      static int gid_xcalo_column_index
+		= geometry_manager_->get_id_mgr ().get_category_info ("xcalo_block").get_subaddress_index ("column");
+	      static int gid_xcalo_row_index
+		= geometry_manager_->get_id_mgr ().get_category_info ("xcalo_block").get_subaddress_index ("row");
+	      truecaloid.push_back(the_scin_hit.get_hit_id());
+
+	      truecalox.push_back(the_scin_hit.get_position_start ().x () / CLHEP::cm);
+	      truecaloy.push_back(the_scin_hit.get_position_start ().y () / CLHEP::cm);
+	      truecaloz.push_back(the_scin_hit.get_position_start ().z () / CLHEP::cm);
+	      truecalotime.push_back(the_scin_hit.get_time_start ()     / CLHEP::ns);
+	      truecaloenergy.push_back(the_scin_hit.get_energy_deposit () / CLHEP::MeV);
+
+	      truecalotype.push_back(1);
+	      truecalomodule.push_back(the_scin_hit.get_geom_id ().get (gid_xcalo_module_index));
+	      truecaloside.push_back(the_scin_hit.get_geom_id ().get (gid_xcalo_side_index));
+	      truecalowall.push_back(the_scin_hit.get_geom_id ().get (gid_xcalo_wall_index));
+	      truecalocolumn.push_back(the_scin_hit.get_geom_id ().get (gid_xcalo_column_index));
+	      truecalorow.push_back(the_scin_hit.get_geom_id ().get (gid_xcalo_row_index));
+	    }
+	}
+
+      if (SD.has_step_hits ("gveto"))
+	{
+	  truecalo_.nohits_ += SD.get_number_of_step_hits ("gveto");
+	  for (unsigned int ihit = 0; ihit < SD.get_number_of_step_hits ("gveto"); ihit++)
+	    {
+	      const mctools::base_step_hit& the_scin_hit = SD.get_step_hit ("gveto", ihit);
+
+	      static int gid_gveto_module_index
+		= geometry_manager_->get_id_mgr ().get_category_info ("gveto_block").get_subaddress_index ("module");
+	      static int gid_gveto_side_index
+		= geometry_manager_->get_id_mgr ().get_category_info ("gveto_block").get_subaddress_index ("side");
+	      static int gid_gveto_wall_index
+		= geometry_manager_->get_id_mgr ().get_category_info ("gveto_block").get_subaddress_index ("wall");
+	      static int gid_gveto_column_index
+		= geometry_manager_->get_id_mgr ().get_category_info ("gveto_block").get_subaddress_index ("column");
+
+	      truecaloid.push_back(the_scin_hit.get_hit_id());
+	      truecalox.push_back(the_scin_hit.get_position_start ().x () / CLHEP::cm);
+	      truecaloy.push_back(the_scin_hit.get_position_start ().y () / CLHEP::cm);
+	      truecaloz.push_back(the_scin_hit.get_position_start ().z () / CLHEP::cm);
+	      truecalotime.push_back(the_scin_hit.get_time_start ()     / CLHEP::ns);
+	      truecaloenergy.push_back(the_scin_hit.get_energy_deposit () / CLHEP::MeV);
+
+	      truecalotype.push_back(2);
+	      truecalorow.push_back(0);
+	      truecalomodule.push_back(the_scin_hit.get_geom_id ().get (gid_gveto_module_index));
+	      truecaloside.push_back(the_scin_hit.get_geom_id ().get (gid_gveto_side_index));
+	      truecalowall.push_back(the_scin_hit.get_geom_id ().get (gid_gveto_wall_index));
+	      truecalocolumn.push_back(the_scin_hit.get_geom_id ().get (gid_gveto_column_index));
+	    }
+	}
+
+      truecalo_.id_ = &truecaloid;
+      truecalo_.type_ = &truecalotype;
+      truecalo_.module_ = &truecalomodule;
+      truecalo_.side_ = &truecaloside;
+      truecalo_.column_ = &truecalocolumn;
+      truecalo_.row_ = &truecalorow;
+      truecalo_.wall_ = &truecalowall;
+      truecalo_.x_ = &truecalox;
+      truecalo_.y_ = &truecaloy;
+      truecalo_.z_ = &truecaloz;
+      truecalo_.time_ = &truecalotime;
+      truecalo_.energy_ = &truecaloenergy;
+
     }
+
+  // look for calibrated data
+  if(workItem.has("CD"))
+    {
+      const snemo::datamodel::calibrated_data & CD = workItem.get<snemo::datamodel::calibrated_data>("CD");
+      //      std::cout << "In process: found CD data bank " << std::endl;
+      tracker_.nohits_ = CD.calibrated_tracker_hits ().size ();
+      BOOST_FOREACH (const snemo::datamodel::calibrated_data::tracker_hit_handle_type & gg_handle, CD.calibrated_tracker_hits ())
+	{
+	  if (! gg_handle.has_data ()) continue;
+
+	  const snemo::datamodel::calibrated_tracker_hit & sncore_gg_hit = gg_handle.get ();
+
+	  trackerid.push_back(sncore_gg_hit.get_hit_id());
+	  trackermodule.push_back(sncore_gg_hit.get_geom_id().get(0));
+	  trackerside.push_back(sncore_gg_hit.get_geom_id().get(1));
+	  trackerlayer.push_back(sncore_gg_hit.get_geom_id().get(2));
+	  trackercolumn.push_back(sncore_gg_hit.get_geom_id().get(3));
+	  trackerx.push_back(sncore_gg_hit.get_x());
+	  trackery.push_back(sncore_gg_hit.get_y());
+	  trackerz.push_back(sncore_gg_hit.get_z());
+	  trackersigmaz.push_back(sncore_gg_hit.get_sigma_z());
+	  trackerr.push_back(sncore_gg_hit.get_r());
+	  trackersigmar.push_back(sncore_gg_hit.get_sigma_r());
+	  trackertruehitid.push_back(sncore_gg_hit.get_id());
+	}
+      tracker_.id_ = &trackerid;
+      tracker_.module_ = &trackermodule;
+      tracker_.side_ = &trackerside;
+      tracker_.layer_ = &trackerlayer;
+      tracker_.column_ = &trackercolumn;
+      tracker_.x_ = &trackerx;
+      tracker_.y_ = &trackery;
+      tracker_.z_ = &trackerz;
+      tracker_.sigmaz_ = &trackersigmaz;
+      tracker_.r_ = &trackerr;
+      tracker_.sigmar_ = &trackersigmar;
+      tracker_.truehitid_ = &trackertruehitid;
+
+      calo_.nohits_ = CD.calibrated_calorimeter_hits ().size ();
+      BOOST_FOREACH (const snemo::datamodel::calibrated_data::calorimeter_hit_handle_type & the_calo_hit_handle, CD.calibrated_calorimeter_hits ())
+	{
+	  if(! the_calo_hit_handle.has_data() ) continue;
+
+	  const snemo::datamodel::calibrated_calorimeter_hit & the_calo_hit = the_calo_hit_handle.get ();
+
+	  caloid.push_back(the_calo_hit.get_hit_id ());
+	  calomodule.push_back(the_calo_hit.get_geom_id ().get (0));
+	  caloside.push_back(the_calo_hit.get_geom_id ().get (1));
+
+	  if (the_calo_hit.get_geom_id ().get_type () == 1302)
+	    {
+	      // CALO
+	      calowall.push_back(0);
+	      calocolumn.push_back(the_calo_hit.get_geom_id ().get (2));
+	      calorow.push_back(the_calo_hit.get_geom_id ().get (3));
+	      calotype.push_back(0);
+	    }
+	  if (the_calo_hit.get_geom_id ().get_type () == 1232)
+	    {
+	      // XCALO
+	      calowall.push_back(the_calo_hit.get_geom_id ().get (2));
+	      calocolumn.push_back(the_calo_hit.get_geom_id ().get (3));
+	      calorow.push_back(the_calo_hit.get_geom_id ().get (0));
+	      calotype.push_back(1);
+	    }
+	  if (the_calo_hit.get_geom_id ().get_type () == 1252)
+	    {
+	      // GVETO
+	      calowall.push_back(the_calo_hit.get_geom_id ().get (2));
+	      calocolumn.push_back(the_calo_hit.get_geom_id ().get (3));
+	      calorow.push_back(0);
+	      calotype.push_back(2);
+	    }
+
+	  calotime.push_back(the_calo_hit.get_time());
+	  calosigmatime.push_back(the_calo_hit.get_sigma_time());
+	  caloenergy.push_back(the_calo_hit.get_energy());
+	  calosigmaenergy.push_back(the_calo_hit.get_sigma_energy());
+	}
+      calo_.id_ = &caloid;
+      calo_.type_ = &calotype;
+      calo_.module_ = &calomodule;
+      calo_.side_ = &caloside;
+      calo_.column_ = &calocolumn;
+      calo_.row_ = &calorow;
+      calo_.wall_ = &calowall;
+      calo_.time_ = &calotime;
+      calo_.sigmatime_ = &calosigmatime;
+      calo_.energy_ = &caloenergy;
+      calo_.sigmaenergy_ = &calosigmaenergy;
+
+    }
+  // look for event header
+  if(workItem.has("EH"))
+    {
+      const snemo::datamodel::event_header & EH = workItem.get<snemo::datamodel::event_header>("EH");
+      //      std::cout << "In process: found EH event header " << std::endl;
+      header_.runnumber_ = EH.get_id ().get_run_number ();
+      header_.eventnumber_ = EH.get_id ().get_event_number ();
+      header_.date_ = 0;
+      header_.runtype_ = 0;
+      header_.simulated_ = (EH.is_simulated () ? true : false);
+    }
+
+  tree_->Fill();
 
   // MUST return a status, see ref dpp::processing_status_flags_type
   return dpp::base_module::PROCESS_OK;
@@ -228,6 +546,7 @@ void Things2Root::reset() {
   hfile_->cd();
   tree_->Write();
   hfile_->Close(); //
+  std::cout << "In reset: finished conversion, file closed " << std::endl;
 
   // clean up
   delete hfile_;
--- modules/things2root/Things2Root.h
+++ modules/things2root/Things2Root.h
@@ -12,6 +12,7 @@
 // Third Party
 #include "TFile.h"
 #include "TTree.h"
+#include <boost/foreach.hpp>
 
 // - Bayeux
 #include "bayeux/dpp/base_module.h"
@@ -20,7 +21,50 @@
 #include "bayeux/geomtools/manager.h"
 #include "bayeux/geomtools/geometry_service.h"
 
+// - Falaise
+#include "falaise/snemo/datamodels/calibrated_data.h"
+#include "falaise/snemo/datamodels/event_header.h"
+
 // This Project
+typedef struct HeaderEventStorage{
+  int runnumber_;
+  int eventnumber_;
+  int date_;
+  int runtype_;
+  bool simulated_;
+} headereventstorage;
+
+typedef struct TrackerEventStorage{
+  int nohits_;
+  std::vector<int>* id_;
+  std::vector<int>* module_;
+  std::vector<int>* side_;
+  std::vector<int>* layer_;
+  std::vector<int>* column_;
+  std::vector<double>* x_;
+  std::vector<double>* y_;
+  std::vector<double>* z_;
+  std::vector<double>* sigmaz_;
+  std::vector<double>* r_;
+  std::vector<double>* sigmar_;
+  std::vector<int>* truehitid_;
+} trackereventstorage;
+
+typedef struct CaloEventStorage{
+  int nohits_;
+  std::vector<int>* id_;
+  std::vector<int>* type_;
+  std::vector<int>* module_;
+  std::vector<int>* side_;
+  std::vector<int>* column_;
+  std::vector<int>* row_;
+  std::vector<int>* wall_;
+  std::vector<double>* time_;
+  std::vector<double>* sigmatime_;
+  std::vector<double>* energy_;
+  std::vector<double>* sigmaenergy_;
+} caloeventstorage;
+
 typedef struct TrueVertexStorage{
   double x_;
   double y_;
@@ -38,6 +82,22 @@ typedef struct TrueParticleStorage{
   std::vector<double>* time_;
   std::vector<double>* ke_;
 } TrueParticleStorage;
+
+typedef struct TrueCaloStorage{
+  int nohits_;
+  std::vector<int>* id_;
+  std::vector<int>* type_;
+  std::vector<int>* module_;
+  std::vector<int>* side_;
+  std::vector<int>* column_;
+  std::vector<int>* row_;
+  std::vector<int>* wall_;
+  std::vector<double>* x_;
+  std::vector<double>* y_;
+  std::vector<double>* z_;
+  std::vector<double>* time_;
+  std::vector<double>* energy_;
+} truecalostorage;
 
 typedef struct TrueTrackerStorage{
   int nohits_;
@@ -61,14 +121,18 @@ class Things2Root : public dpp::base_module {
  public:
   //! Construct module
   Things2Root();
+
   //! Destructor
   virtual ~Things2Root();
+
   //! Configure the module
   virtual void initialize(const datatools::properties& myConfig,
 			  datatools::service_manager& flServices,
 			  dpp::module_handle_dict_type& what);
+
   //! Process supplied data record
   virtual dpp::base_module::process_status process(datatools::things& workItem);
+
   //! Reset the module
   virtual void reset();
 
@@ -83,9 +147,14 @@ class Things2Root : public dpp::base_module {
   TFile* hfile_;
   TTree* tree_;
 
+  TrueCaloStorage truecalo_; // see typedefs
   TrueTrackerStorage truetracker_; // see typedefs
   TrueVertexStorage truevertex_; // see typedefs
   TrueParticleStorage trueparticle_; // see typedefs
+
+  HeaderEventStorage header_;
+  TrackerEventStorage tracker_;
+  CaloEventStorage calo_;
 
   // Macro which automatically creates the interface needed
   // to enable the module to be loaded at runtime
