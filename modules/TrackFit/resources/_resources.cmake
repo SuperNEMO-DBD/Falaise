e9b8718b7c9496fce76df94b3af3b0c8dfb8f7c2
Francois Mauger <mauger@lpccaen.in2p3.fr>
2014-02-27 23:55:20 +0000

    reorganize resource files

---
+++ modules/TrackFit/resources/_resources.cmake
@@ -0,0 +1,6 @@
+list(APPEND _falaise_TrackFit_RESOURCES_FILES
+${PROJECT_SOURCE_DIR}/modules/TrackFit/resources/config/snemo/reconstruction/examples/FalaiseTrackFitPlugin.example.conf
+${PROJECT_SOURCE_DIR}/modules/TrackFit/resources/config/snemo/legacy/modules_tracker_fitting.conf.in
+${PROJECT_SOURCE_DIR}/modules/TrackFit/resources/config/snemo/legacy/modules_tracker_clustering.conf.in
+${PROJECT_SOURCE_DIR}/modules/TrackFit/resources/config/snemo/legacy/modules_mock_digi_calib.conf.in
+)
