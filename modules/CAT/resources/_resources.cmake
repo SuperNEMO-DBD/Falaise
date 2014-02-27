0abfbc08dba6cb931320053424719f37e84ee359
Francois Mauger <mauger@lpccaen.in2p3.fr>
2014-02-27 23:28:29 +0000

    reorganize CAT resource files

---
+++ modules/CAT/resources/_resources.cmake
@@ -0,0 +1,4 @@
+list(APPEND _falaise_CAT_RESOURCES_FILES
+${PROJECT_SOURCE_DIR}/modules/CAT/resources/config/snemo/common/reconstruction/examples/FalaiseCATPlugin.example.conf
+${PROJECT_SOURCE_DIR}/modules/CAT/resources/config/snemo/common/reconstruction/1.0/FalaiseCATPlugin.conf
+)
