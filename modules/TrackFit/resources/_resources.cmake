f0d6afc59e8f583d90ac0f6e6bb109bdd11c8249
Francois Mauger <mauger@lpccaen.in2p3.fr>
2014-03-07 00:07:17 +0000

    update resource files support in TrackFit plugin

--- modules/TrackFit/resources/_resources.cmake
+++ modules/TrackFit/resources/_resources.cmake
@@ -3,4 +3,5 @@ ${PROJECT_SOURCE_DIR}/modules/TrackFit/resources/config/snemo/legacy/modules_tra
 ${PROJECT_SOURCE_DIR}/modules/TrackFit/resources/config/snemo/legacy/modules_tracker_clustering.conf.in
 ${PROJECT_SOURCE_DIR}/modules/TrackFit/resources/config/snemo/legacy/modules_mock_digi_calib.conf.in
 ${PROJECT_SOURCE_DIR}/modules/TrackFit/resources/config/snemo/common/reconstruction/examples/FalaiseTrackFitPlugin.example.conf
+${PROJECT_SOURCE_DIR}/modules/TrackFit/resources/config/snemo/common/reconstruction/1.0/trackfit.conf
 )
