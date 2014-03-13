8043cf88153adcfd30d67ad87a6a8283e21dbbcc
Francois Mauger <mauger@lpccaen.in2p3.fr>
2014-03-13 23:41:01 +0000

    reorganize TrackFit plugin resource files

--- modules/TrackFit/resources/_resources.cmake
+++ modules/TrackFit/resources/_resources.cmake
@@ -1,7 +1,4 @@
 list(APPEND _falaise_TrackFit_RESOURCES_FILES
-${PROJECT_SOURCE_DIR}/modules/TrackFit/resources/config/snemo/legacy/modules_tracker_fitting.conf.in
-${PROJECT_SOURCE_DIR}/modules/TrackFit/resources/config/snemo/legacy/modules_tracker_clustering.conf.in
-${PROJECT_SOURCE_DIR}/modules/TrackFit/resources/config/snemo/legacy/modules_mock_digi_calib.conf.in
-${PROJECT_SOURCE_DIR}/modules/TrackFit/resources/config/snemo/common/reconstruction/examples/FalaiseTrackFitPlugin.example.conf
+${PROJECT_SOURCE_DIR}/modules/TrackFit/resources/examples/config/FalaiseTrackFitPlugin.example.conf
 ${PROJECT_SOURCE_DIR}/modules/TrackFit/resources/config/snemo/common/reconstruction/1.0/trackfit.conf
 )
