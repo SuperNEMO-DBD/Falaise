1a46f31b5a7d142cccedef764da21c861748eb2b
Francois Mauger <mauger@lpccaen.in2p3.fr>
2014-03-14 00:12:18 +0000

    Reorganize the CAT plugin resource files

--- modules/CAT/resources/_resources.cmake
+++ modules/CAT/resources/_resources.cmake
@@ -1,6 +1,6 @@
 list(APPEND _falaise_CAT_RESOURCES_FILES
-${PROJECT_SOURCE_DIR}/modules/CAT/resources/config/snemo/common/reconstruction/examples/FalaiseCATPlugin.example.conf
+${PROJECT_SOURCE_DIR}/modules/CAT/resources/examples/config/FalaiseCATPlugin.example_1.conf
+${PROJECT_SOURCE_DIR}/modules/CAT/resources/examples/config/FalaiseCATPlugin.example_2.conf
 ${PROJECT_SOURCE_DIR}/modules/CAT/resources/config/snemo/common/reconstruction/1.0/SULTAN.conf
-${PROJECT_SOURCE_DIR}/modules/CAT/resources/config/snemo/common/reconstruction/1.0/FalaiseCATPlugin.conf
 ${PROJECT_SOURCE_DIR}/modules/CAT/resources/config/snemo/common/reconstruction/1.0/CAT.conf
 )
