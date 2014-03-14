7c36e70c1deaf78dd264d754ccc40d408c1351d2
Francois Mauger <mauger@lpccaen.in2p3.fr>
2014-03-14 00:24:19 +0000

    Publishing resource files for the MockTrackerClusterizer plugin

---
+++ modules/MockTrackerClusterizer/resources/_resources.cmake
@@ -0,0 +1,3 @@
+list(APPEND _falaise_MockTrackerClusterizer_RESOURCES_FILES
+${PROJECT_SOURCE_DIR}/modules/MockTrackerClusterizer/resources/config/snemo/common/reconstruction/1.0/mock_tracker_clusterizer.conf
+)
