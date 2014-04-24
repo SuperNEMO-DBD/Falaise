66d3733dfb2f1c27d81eff9e1b7b64c9913a842d
Francois Mauger <mauger@lpccaen.in2p3.fr>
2014-04-24 09:22:50 +0000

    Update resource files in the MockTrackerClusterizer module
    The example ``ex01`` is provided to illustrate the use of
    a simple reconstruction pipeline.

--- modules/MockTrackerClusterizer/resources/_resources.cmake
+++ modules/MockTrackerClusterizer/resources/_resources.cmake
@@ -1,3 +1,5 @@
 list(APPEND _falaise_MockTrackerClusterizer_RESOURCES_FILES
+${PROJECT_SOURCE_DIR}/modules/MockTrackerClusterizer/resources/examples/ex01/config/pipeline.conf
+${PROJECT_SOURCE_DIR}/modules/MockTrackerClusterizer/resources/examples/ex01/README.rst
 ${PROJECT_SOURCE_DIR}/modules/MockTrackerClusterizer/resources/config/snemo/common/reconstruction/1.0/mock_tracker_clusterizer.conf
 )
