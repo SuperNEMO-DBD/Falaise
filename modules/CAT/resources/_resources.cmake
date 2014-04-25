929973195aaaf2da011b521099c5ebf6285649c4
Francois Mauger <mauger@lpccaen.in2p3.fr>
2014-04-25 11:46:20 +0000

    Update CAT resource file list

--- modules/CAT/resources/_resources.cmake
+++ modules/CAT/resources/_resources.cmake
@@ -1,6 +1,9 @@
 list(APPEND _falaise_CAT_RESOURCES_FILES
-${PROJECT_SOURCE_DIR}/modules/CAT/resources/examples/config/FalaiseCATPlugin.example_1.conf
-${PROJECT_SOURCE_DIR}/modules/CAT/resources/examples/config/FalaiseCATPlugin.example_2.conf
+${PROJECT_SOURCE_DIR}/modules/CAT/resources/examples/README.rst
+${PROJECT_SOURCE_DIR}/modules/CAT/resources/examples/ex01/config/pipeline.conf
+${PROJECT_SOURCE_DIR}/modules/CAT/resources/examples/ex01/README.rst
+${PROJECT_SOURCE_DIR}/modules/CAT/resources/examples/ex02/config/pipeline.conf
+${PROJECT_SOURCE_DIR}/modules/CAT/resources/examples/ex02/README.rst
 ${PROJECT_SOURCE_DIR}/modules/CAT/resources/config/snemo/common/reconstruction/1.0/SULTAN.conf
 ${PROJECT_SOURCE_DIR}/modules/CAT/resources/config/snemo/common/reconstruction/1.0/CAT.conf
 )
