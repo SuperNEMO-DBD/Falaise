f976ad43163dc2c83cdd006401d0e50291cb3bba
Xavier Garrido <garrido@lal.in2p3.fr>
2014-07-09 16:06:40 +0000

    store string label within auxiliaries property

--- source/falaise/snemo/testing/test_snemo_datamodel_particle_track_data.cxx
+++ source/falaise/snemo/testing/test_snemo_datamodel_particle_track_data.cxx
@@ -19,40 +19,6 @@ int main (/*int argc_, char ** argv_*/)
     {
       std::clog << "Test program for the 'particle_track_data' class." << std::endl;
 
-      // bool debug = false;
-      // bool verbose = false;
-
-      // int iarg = 1;
-      // while (iarg < argc_)
-      //   {
-      //     string token = argv_[iarg];
-
-      //     if (token[0] == '-')
-      //       {
-      //         string option = token;
-      //         if ((option == "-d") || (option == "--debug"))
-      //           {
-      //             debug = true;
-      //           }
-      //         else if ((option == "-v") || (option == "--verbose"))
-      //           {
-      //             verbose = true;
-      //           }
-      //         else
-      //           {
-      //             clog << "warning: ignoring option '" << option << "'!" << endl;
-      //           }
-      //       }
-      //     else
-      //       {
-      //         string argument = token;
-      //         {
-      //           clog << "warning: ignoring argument '" << argument << "'!" << endl;
-      //         }
-      //       }
-      //     iarg++;
-      //   }
-
       namespace sdm = snemo::datamodel;
 
       // Create a handle on some trajectory pattern :
@@ -78,24 +44,32 @@ int main (/*int argc_, char ** argv_*/)
       // Create a handle of fake vertices :
       datatools::handle<geomtools::blur_spot> hV0;
       hV0.reset (new geomtools::blur_spot (geomtools::blur_spot::dimension_three));
+      hV0.grab ().set_hit_id(0);
+      hV0.grab ().grab_geom_id().set_type(1102);     // "source_strip" geometry category
+      hV0.grab ().grab_geom_id().set_address(0, 23); // module #0, source strip #23
       hV0.grab ().set_position (geomtools::vector_3d (1.0 * CLHEP::mm,
                                                      2.0 * CLHEP::mm,
                                                      3.0 * CLHEP::mm));
       hV0.grab ().set_x_error (0.5 * CLHEP::mm);
       hV0.grab ().set_y_error (0.5 * CLHEP::mm);
       hV0.grab ().set_z_error (0.5 * CLHEP::mm);
-      hV0.grab ().grab_auxiliaries ().store_flag ("vertex_on_foil");
+      hV0.grab ().grab_auxiliaries ().store(sdm::particle_track::vertex_type_key(),
+                                            sdm::particle_track::vertex_on_source_foil_label());
       hV0.get ().tree_dump (std::clog, "Foil vertex : ");
 
       datatools::handle<geomtools::blur_spot> hV1;
       hV1.reset (new geomtools::blur_spot (geomtools::blur_spot::dimension_three));
+      hV1.grab ().set_hit_id(1);
+      hV1.grab ().grab_geom_id().set_type(1302);          // "calorimeter_block" geometry category
+      hV1.grab ().grab_geom_id().set_address(0, 1, 4, 7); // module #0, wall #1, column #4, row #7
       hV1.grab ().set_position (geomtools::vector_3d (10.0 * CLHEP::mm,
                                                      20.0 * CLHEP::mm,
                                                      30.0 * CLHEP::mm));
       hV1.grab ().set_x_error (2.5 * CLHEP::mm);
       hV1.grab ().set_y_error (2.5 * CLHEP::mm);
       hV1.grab ().set_z_error (2.5 * CLHEP::mm);
-      hV1.grab ().grab_auxiliaries ().store_flag ("vertex_on_calorimeter");
+      hV1.grab ().grab_auxiliaries ().store(sdm::particle_track::vertex_type_key(),
+                                            sdm::particle_track::vertex_on_main_calorimeter_label());
       hV1.get ().tree_dump (std::clog, "Calorimeter vertex : ");
 
       // Create the particle track :
