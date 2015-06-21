4141e2bff5e1c4c90ba18e97ad71b418e3f9f895
Francois Mauger <mauger@lpccaen.in2p3.fr>
2015-06-21 20:30:17 +0000

    Fix test program in the Digitization module

--- modules/Digitization/testing/test_geiger_tp_data.cxx
+++ modules/Digitization/testing/test_geiger_tp_data.cxx
@@ -16,12 +16,12 @@
 // This project :
 #include <snemo/digitization/geiger_tp_data.h>
 
-int main(int argc_, char **  argv_)
+int main(int /* argc_ */, char ** /* argv_ */)
 {
   FALAISE_INIT();
   int error_code = EXIT_SUCCESS;
   datatools::logger::priority logging = datatools::logger::PRIO_FATAL;
-  try {   
+  try {
     std::clog << "Test program for class 'snemo::digitization::geiger_tp_data' !" << std::endl;
 
     snemo::digitization::geiger_tp_data my_geiger_tp_data;
@@ -32,7 +32,7 @@ int main(int argc_, char **  argv_)
       snemo::digitization::geiger_tp & gg_tp = my_geiger_tp_data.add();
       gg_tp.set_hit_id(12);
       gg_tp.grab_geom_id().set_type(42);
-      // gg_tp.grab_geom_id().set_address(3,1,7);
+      gg_tp.grab_geom_id().set_address(3,1,7);
       gg_tp.grab_auxiliaries().store("author", "guillaume");
       gg_tp.grab_auxiliaries().store_flag("mock");
       gg_tp.set_clocktick_800ns(15);
@@ -42,6 +42,7 @@ int main(int argc_, char **  argv_)
       snemo::digitization::geiger_tp & gg_tp = my_geiger_tp_data.add();
       gg_tp.set_hit_id(17);
       gg_tp.grab_geom_id().set_type(42);
+      gg_tp.grab_geom_id().set_address(3,2,7);
       gg_tp.grab_auxiliaries().store("author", "guillaume");
       gg_tp.grab_auxiliaries().store_flag("mock");
       gg_tp.set_clocktick_800ns(15);
@@ -51,6 +52,7 @@ int main(int argc_, char **  argv_)
       snemo::digitization::geiger_tp & gg_tp = my_geiger_tp_data.add();
       gg_tp.set_hit_id(17);
       gg_tp.grab_geom_id().set_type(42);
+      gg_tp.grab_geom_id().set_address(3,2,8);
       gg_tp.grab_auxiliaries().store("author", "guillaume");
       gg_tp.grab_auxiliaries().store_flag("mock");
       gg_tp.set_clocktick_800ns(18);
@@ -60,6 +62,7 @@ int main(int argc_, char **  argv_)
       snemo::digitization::geiger_tp & gg_tp = my_geiger_tp_data.add();
       gg_tp.set_hit_id(17);
       gg_tp.grab_geom_id().set_type(42);
+      gg_tp.grab_geom_id().set_address(3,3,8);
       gg_tp.grab_auxiliaries().store("author", "guillaume");
       gg_tp.grab_auxiliaries().store_flag("mock");
       gg_tp.set_clocktick_800ns(20);
@@ -69,6 +72,7 @@ int main(int argc_, char **  argv_)
       snemo::digitization::geiger_tp & gg_tp = my_geiger_tp_data.add();
       gg_tp.set_hit_id(17);
       gg_tp.grab_geom_id().set_type(42);
+      gg_tp.grab_geom_id().set_address(3,4,6);
       gg_tp.grab_auxiliaries().store("author", "guillaume");
       gg_tp.grab_auxiliaries().store_flag("mock");
       gg_tp.set_clocktick_800ns(20);
@@ -81,15 +85,15 @@ int main(int argc_, char **  argv_)
 
     // Creation of a list of geiger tp per clocktick per crate
     std::vector<datatools::handle<snemo::digitization::geiger_tp> > my_list_of_gg_tp_per_clocktick_per_crate;
-    
-    // For this example, 20 is the clocktick and 0 is the number of crate 
-    my_geiger_tp_data.get_list_of_gg_tp_per_clocktick_per_crate(20, 0, my_list_of_gg_tp_per_clocktick_per_crate); 
-    
+
+    // For this example, 20 is the clocktick and 0 is the number of crate
+    my_geiger_tp_data.get_list_of_gg_tp_per_clocktick_per_crate(20, 0, my_list_of_gg_tp_per_clocktick_per_crate);
+
     std::clog << "Size of my list of geiger tp for a clocktick = 20 and crate number = 0 : " << my_list_of_gg_tp_per_clocktick_per_crate.size() << " geiger tp(s) in the list " << std::endl;
 
-    for (int i = 0; i < my_list_of_gg_tp_per_clocktick_per_crate.size(); i++)
+    for (int i = 0; i < (int) my_list_of_gg_tp_per_clocktick_per_crate.size(); i++)
       {
-	my_list_of_gg_tp_per_clocktick_per_crate[i].get().tree_dump(std::clog, "My list of geiger TP per clocktick per crate" , "INFO : ");
+        my_list_of_gg_tp_per_clocktick_per_crate[i].get().tree_dump(std::clog, "My list of geiger TP per clocktick per crate" , "INFO : ");
       }
     my_geiger_tp_data.tree_dump(std::clog, "my_geiger_TP_data : ", "INFO : ");
 
@@ -109,4 +113,3 @@ int main(int argc_, char **  argv_)
   FALAISE_FINI();
   return error_code;
 }
-
