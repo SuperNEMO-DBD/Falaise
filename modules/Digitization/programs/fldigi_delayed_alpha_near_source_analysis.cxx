037ee1fb2b7eeb3566d8868e2b13671481f72c96
Guillaume Oliviero <goliviero@lpccaen.in2p3.fr>
2016-01-07 18:28:44 +0000

    Update alpha near source analysis

--- modules/Digitization/programs/fldigi_delayed_alpha_near_source_analysis.cxx
+++ modules/Digitization/programs/fldigi_delayed_alpha_near_source_analysis.cxx
@@ -13,6 +13,9 @@
 // Root : 
 #include "TFile.h"
 #include "TTree.h"
+#include "TH1F.h"
+#include "TH2F.h"
+#include "TH3F.h"
 
 int main(int  argc_ , char ** argv_)
 {  
@@ -24,74 +27,90 @@ int main(int  argc_ , char ** argv_)
   try {  
     std::clog << "Little test program for analysis of alpha delayed number of geiger cells hit !" << std::endl;
     
-    std::string input_file_name = "${FALAISE_DIGITIZATION_DIR}/programs/validation.root";
+    std::string input_filename = "${FALAISE_DIGITIZATION_DIR}/programs/validation.root";
+    std::string output_filename = "${FALAISE_DIGITIZATION_DIR}/programs/analysis.root";
+    datatools::fetch_path_with_env(input_filename);
+    datatools::fetch_path_with_env(output_filename);
 
     // Creation and openning of ROOT file , and get the infile tree :
-    TFile * input_file = new TFile(input_file_name.c_str() , "READ");
+    TFile * input_file = new TFile(input_filename.c_str() , "READ");
+    TFile * output_file = new TFile(output_filename.c_str(), "RECREATE");
 
-    TTree * input_tree = (TTree *)input_file->Get("TrackerTree");
-    Int_t number_of_gg_cells = 0;
-    Int_t number_of_delayed_gg_cells = 0;
-    Int_t number_of_not_delayed_gg_cells = 0;
-    double time_start = 0;
-    Double_t x_start  = 0;
-    Double_t y_start  = 0;
-    Double_t z_start  = 0;
-    Double_t x_stop   = 0;
-    Double_t y_stop   = 0;
-    Double_t z_stop   = 0;
-    Double_t px_start = 0;
-    Double_t py_start = 0;
-    Double_t pz_start = 0;
-    std::vector<double> * vect_time_start = 0;
-    std::vector<Double_t> vect_x_start;
-    std::vector<Double_t> vect_y_start;
-    std::vector<Double_t> vect_z_start;
-    std::vector<Double_t> vect_x_stop;
-    std::vector<Double_t> vect_y_stop;
-    std::vector<Double_t> vect_z_stop;
-    std::vector<Double_t> vect_px_start;
-    std::vector<Double_t> vect_py_start;
-    std::vector<Double_t> vect_pz_start;
-    Int_t hit_id      = 0; 
+    TTree * input_gg_cells_tree = (TTree *)input_file->Get("GGCellsTree");
+    TTree * input_delayed_cells_tree = (TTree *)input_file->Get("DelayedCellsTree");
+    TTree * output_tree = new TTree("TrackerTreeAnalysis", "Alpha track analysis");
+
     Int_t event_id    = 0;
-    //input_tree->SetBranchAddress("event_id",   &event_id);
-    input_tree->SetBranchAddress("number_of_gg_cells", &number_of_gg_cells);
-    input_tree->SetBranchAddress("number_of_delayed_gg_cells", &number_of_delayed_gg_cells);
-    input_tree->SetBranchAddress("number_ of_not_delayed_gg_cells", &number_of_not_delayed_gg_cells);
-    TBranch *branch_vect_time_start = 0;
-    input_tree->SetBranchAddress("time_start_vector", &vect_time_start, &branch_vect_time_start);
-    // input_tree->SetBranchAddress("x_start",    &x_start);
-    // input_tree->SetBranchAddress("y_start",    &y_start);
-    // input_tree->SetBranchAddress("z_start",    &z_start);
-    // input_tree->SetBranchAddress("x_stop",     &x_stop);
-    // input_tree->SetBranchAddress("y_stop",     &y_stop);
-    // input_tree->SetBranchAddress("z_stop",     &z_stop);
-    // input_tree->SetBranchAddress("px_start",   &px_start);
-    // input_tree->SetBranchAddress("py_start",   &py_start);
-    // input_tree->SetBranchAddress("pz_start",   &pz_start);
-    // input_tree->SetBranchAddress("hit_id",     &hit_id);
+    std::vector<Int_t> * vect_number_of_gg_cells = 0;
+    std::vector<Int_t> * vect_number_of_delayed_gg_cells = 0;
+    std::vector<Int_t> * vect_number_of_not_delayed_gg_cells = 0;
+    std::vector<Int_t> * vect_delayed_time_shift = 0;
+
+    std::vector<Int_t> * vect_hit_id = 0;
+    std::vector<Double_t> * vect_time_start = 0;
+    std::vector<Double_t> * vect_x_start = 0;
+    std::vector<Double_t> * vect_y_start = 0;
+    std::vector<Double_t> * vect_z_start = 0;
+    std::vector<Double_t> * vect_x_stop = 0;
+    std::vector<Double_t> * vect_y_stop = 0;
+    std::vector<Double_t> * vect_z_stop = 0;
+    std::vector<Double_t> * vect_px_start = 0;
+    std::vector<Double_t> * vect_py_start = 0;
+    std::vector<Double_t> * vect_pz_start = 0;
+
+    // Input tree Set all Branch Address :
+    input_delayed_cells_tree->SetBranchAddress("event_id",   &event_id);
+    input_delayed_cells_tree->SetBranchAddress("number_of_gg_cells", &vect_number_of_gg_cells);
+    input_delayed_cells_tree->SetBranchAddress("number_of_delayed_gg_cells", &vect_number_of_delayed_gg_cells);
+    input_delayed_cells_tree->SetBranchAddress("number_of_not_delayed_gg_cells", &vect_number_of_not_delayed_gg_cells);  
+    input_delayed_cells_tree->SetBranchAddress("delayed_time_shift", &vect_delayed_time_shift);
+    input_gg_cells_tree->SetBranchAddress("event_id",        &event_id);
+    input_gg_cells_tree->SetBranchAddress("hit_id_vector",   &vect_hit_id);
+    input_gg_cells_tree->SetBranchAddress("time_start_vector", &vect_time_start);
+    input_gg_cells_tree->SetBranchAddress("x_start_vector",  &vect_x_start);
+    input_gg_cells_tree->SetBranchAddress("y_start_vector",  &vect_y_start);
+    input_gg_cells_tree->SetBranchAddress("z_start_vector",  &vect_z_start);
+    input_gg_cells_tree->SetBranchAddress("x_stop_vector",   &vect_x_stop);
+    input_gg_cells_tree->SetBranchAddress("y_stop_vector",   &vect_y_stop);
+    input_gg_cells_tree->SetBranchAddress("z_stop_vector",   &vect_z_stop);
+    input_gg_cells_tree->SetBranchAddress("px_start_vector", &vect_px_start);
+    input_gg_cells_tree->SetBranchAddress("py_start_vector", &vect_py_start);
+    input_gg_cells_tree->SetBranchAddress("pz_start_vector", &vect_pz_start);
+
+    TH2F * histo_delayed_geiger_cells_delayed_time = new TH2F( "Delayed Geiger Cells" , "Delayed GG cells" , 60 , 0. , 60., 20, 0., 20.) ;
 
     // Begining of the analysis
-    std::clog << "Number of Entries in TTree = " <<  input_tree->GetEntries() << std::endl; 
-    Int_t n_entry = input_tree->GetEntries();
+    std::clog << "Number of Entries in TTree = " <<  input_delayed_cells_tree->GetEntries() << std::endl; 
+    Int_t n_entry = input_delayed_cells_tree->GetEntries();
     
-    for( int i = 0 ; i < input_tree->GetEntries() ; i++ )
+    
+    const int geiger_threshold = 2; // number of geiger cells threshold     
+
+    for( int i = 0 ; i < n_entry; i++ )
       {
-    	input_tree->GetEntry(i) ;
-    	branch_vect_time_start->GetEntry(i);
-    	std::clog << "vect size = " << vect_time_start->size() << std::endl;
-    	// for (int j = 0; j < vect_time_start->size(); j++)
-    	//   {
-    	//     std::clog << "j = " << j << " Time start = " << vect_time_start->at(j) << std::endl;
-    	//   }
+    	input_delayed_cells_tree->GetEntry(i) ;
+     
     	std::clog << "Event ID  = " << event_id << std::endl;
-    	//std::clog << "Hit ID  = "   << hit_id << std::endl;
-    	//std::clog << "Tstart = "    << time_start << std::endl;
-    	std::clog << "Number of gg cells = " << number_of_gg_cells << std::endl;
-    	std::clog << "Number of delayed gg cells = " << number_of_delayed_gg_cells << std::endl;
-    	std::clog << "Number of not delayed gg cells = " << number_of_not_delayed_gg_cells << std::endl << std::endl;
+	size_t vector_size = vect_number_of_gg_cells->size();
+	for (size_t j = 0; j < vector_size; j++)
+	  {
+	    int delayed_time_shift = vect_delayed_time_shift->at(j);
+	    int number_of_gg_cells = vect_number_of_gg_cells->at(j);
+	    int number_of_delayed_gg_cells = vect_number_of_delayed_gg_cells->at(j);
+	    int number_of_not_delayed_gg_cells = vect_number_of_not_delayed_gg_cells->at(j);
+	    histo_delayed_geiger_cells_delayed_time->Fill(delayed_time_shift, number_of_delayed_gg_cells);
+	    // std::clog << "Delayed time shift              = " << delayed_time_shift << std::endl;
+	    // std::clog << "Number of gg cells             = " << number_of_gg_cells << std::endl;
+	    // std::clog << "Number of delayed gg cells     = " << number_of_delayed_gg_cells << std::endl;
+	    // std::clog << "Number of not delayed gg cells = " << number_of_not_delayed_gg_cells << std::endl << std::endl;
+	  }
       }
+    histo_delayed_geiger_cells_delayed_time->Write();
+
+    //output_tree->Fill();
+    output_file->Write();
+    output_file->Close();
+   
     std::clog << "The end." << std::endl;
   }
   
@@ -109,3 +128,34 @@ int main(int  argc_ , char ** argv_)
   FALAISE_FINI();
   return error_code;
 }
+
+
+// for ggcells tree :
+// double px_tot = 0;
+// double py_tot = 0;
+// double pz_tot = 0;
+// double p_tot = 0;
+// for (int j = 0; j < vect_time_start->size(); j++)
+//   {
+//     Int_t hit_id      = vect_hit_id->at(j); 
+//     Double_t time_start = vect_time_start->at(j);
+//     Double_t x_start  = vect_x_start->at(j);
+//     Double_t y_start  = vect_y_start->at(j);
+//     Double_t z_start  = vect_z_start->at(j);
+//     Double_t x_stop   = vect_x_stop->at(j);
+//     Double_t y_stop   = vect_y_stop->at(j);
+//     Double_t z_stop   = vect_z_stop->at(j);
+//     Double_t px_start = vect_px_start->at(j);
+//     Double_t py_start = vect_py_start->at(j);
+//     Double_t pz_start = vect_pz_start->at(j);
+
+//     // std::clog << "Hit id = " << hit_id << std::endl;
+//     // std::clog << "Time start = " << time_start << std::endl;
+//     // std::clog << "XYZ start = " << x_start << " , " <<  y_start << " , " << z_start << std::endl;
+//     // std::clog << "XYZ stop = " << x_stop << " , " <<  y_stop << " , " << z_stop << std::endl;
+//     // std::clog << "PxPyPz start = " << px_start << " , " <<  py_start << " , " << pz_start << std::endl << std::endl;
+//     px_tot += px_start ;
+//     py_tot += py_start;
+//     pz_tot += pz_start;
+//   }
+// p_tot = std::sqrt(std::pow(px_tot, 2) + std::pow(py_tot, 2) + std::pow(pz_tot, 2));
--- modules/Digitization/programs/fldigi_delayed_alpha_near_source_validation.cxx
+++ modules/Digitization/programs/fldigi_delayed_alpha_near_source_validation.cxx
@@ -64,7 +64,8 @@ int main(int  argc_ , char ** argv_)
     if(is_input_file){
       pipeline_simulated_data_filename = input_filename;
     }else{
-      pipeline_simulated_data_filename = "${FALAISE_DIGITIZATION_TESTING_DIR}/data/Se82_0nubb-source_strips_bulk_SD_10_events.brio";
+      //pipeline_simulated_data_filename = "${FALAISE_DIGITIZATION_TESTING_DIR}/data/Se82_0nubb-source_strips_bulk_SD_10_events.brio";
+      pipeline_simulated_data_filename = "/home/guillaume/software/my_falaise/outputs/Bi214_Po214_100000-field_wire_surface.brio";
       // pipeline_simulated_data_filename = "${DATA_NEMO_PERSO_DIR}/trigger/simulated_data_brio/Bi214_Po214_500000-source_strips_bulk_SD.brio";
       //pipeline_simulated_data_filename = "${DATA_NEMO_PERSO_DIR}/trigger/simulated_data_brio/Se82_0nubb_500000-source_strips_bulk_SD.brio";
       //pipeline_simulated_data_filename = "${DATA_NEMO_PERSO_DIR}/trigger/simulated_data_brio/Bi214_Po214_500000-field_wire_surface_SD.brio";
@@ -75,7 +76,7 @@ int main(int  argc_ , char ** argv_)
     dpp::input_module reader;
     datatools::properties reader_config;
     reader_config.store ("logging.priority", "debug");
-    reader_config.store ("max_record_total", 1);
+    reader_config.store ("max_record_total", 10000);
     reader_config.store ("files.mode", "single");
     reader_config.store ("files.single.filename", pipeline_simulated_data_filename);
     reader.initialize_standalone (reader_config);
@@ -84,11 +85,18 @@ int main(int  argc_ , char ** argv_)
     std::string root_filename = "${FALAISE_DIGITIZATION_DIR}/programs/validation.root";
     datatools::fetch_path_with_env(root_filename);
     TFile* root_file = new TFile(root_filename.c_str(), "RECREATE");
-    TTree* tree = new TTree("TrackerTree", "Alpha track analysis");
-    Int_t number_of_gg_cells = 0;
-    Int_t number_of_delayed_gg_cells = 0;
-    Int_t number_of_not_delayed_gg_cells = 0;
-    std::vector<double> vect_time_start;
+
+    TTree* gg_cells_tree = new TTree("GGCellsTree", "Alpha track analysis");
+    TTree* delayed_cells_tree = new TTree("DelayedCellsTree", "Alpha track analysis");
+
+    // Variables definitions :
+    Int_t event_id    = 0;
+    std::vector<Int_t> vect_number_of_gg_cells;
+    std::vector<Int_t> vect_number_of_delayed_gg_cells;
+    std::vector<Int_t> vect_number_of_not_delayed_gg_cells;
+    std::vector<Int_t> vect_delayed_time_shift;
+    std::vector<Int_t> vect_hit_id;
+    std::vector<Double_t> vect_time_start;
     std::vector<Double_t> vect_x_start;
     std::vector<Double_t> vect_y_start;
     std::vector<Double_t> vect_z_start;
@@ -98,7 +106,12 @@ int main(int  argc_ , char ** argv_)
     std::vector<Double_t> vect_px_start;
     std::vector<Double_t> vect_py_start;
     std::vector<Double_t> vect_pz_start;
-    std::vector<double> * ptr_time_start = & vect_time_start;
+    std::vector<Int_t> * ptr_number_of_gg_cells = & vect_number_of_gg_cells;
+    std::vector<Int_t> * ptr_number_of_delayed_gg_cells = & vect_number_of_delayed_gg_cells;
+    std::vector<Int_t> * ptr_number_of_not_delayed_gg_cells = & vect_number_of_not_delayed_gg_cells;
+    std::vector<Int_t> * ptr_delayed_time_shift = & vect_delayed_time_shift;
+    std::vector<Int_t> * ptr_hit_id = & vect_hit_id;
+    std::vector<Double_t> * ptr_time_start = & vect_time_start;
     std::vector<Double_t> * ptr_x_start = & vect_x_start;
     std::vector<Double_t> * ptr_y_start = & vect_y_start;
     std::vector<Double_t> * ptr_z_start = & vect_z_start;
@@ -109,27 +122,27 @@ int main(int  argc_ , char ** argv_)
     std::vector<Double_t> * ptr_py_start = & vect_py_start;
     std::vector<Double_t> * ptr_pz_start = & vect_pz_start;
 
-    std::vector<Int_t> vect_hit_id;
-    std::vector<Int_t> * ptr_hit_id = & vect_hit_id;
-  
-    Int_t event_id    = 0;
-
-    tree->Branch("event_id_vector", &event_id, "event_id/I");
-    tree->Branch("number_of_gg_cells", &number_of_gg_cells, "number_of_gg_cells/I");
-    tree->Branch("number_of_delayed_gg_cells", &number_of_delayed_gg_cells, "number_of_delayed_gg_cells/I");
-    tree->Branch("number_ of_not_delayed_gg_cells", &number_of_not_delayed_gg_cells, "number_of_not_delayed_gg_cells/I");
-    tree->Branch("hit_id_vector",     &ptr_hit_id, "hit_id/I");
-    tree->Branch("time_start_vector", &vect_time_start);//, "time_start/D");
-    tree->Branch("x_start_vector",    &ptr_x_start, "x_start/D");
-    tree->Branch("y_start_vector",    &ptr_y_start, "y_start/D");
-    tree->Branch("z_start_vector",    &ptr_z_start, "z_start/D");
-    tree->Branch("x_stop_vector",     &ptr_x_stop, "x_stop/D");
-    tree->Branch("y_stop_vector",     &ptr_y_stop, "y_stop/D");
-    tree->Branch("z_stop_vector",     &ptr_z_stop, "z_stop/D");
-    tree->Branch("px_start_vector",   &ptr_px_start, "px_start/D");
-    tree->Branch("py_start_vector",   &ptr_py_start, "py_start/D");
-    tree->Branch("pz_start_vector",   &ptr_pz_start, "pz_start/D");
-
+    // Branch definitions : 
+    gg_cells_tree->Branch("event_id", &event_id, "event_id/I");
+    delayed_cells_tree->Branch("event_id", &event_id);//, "event_id/I");
+    delayed_cells_tree->Branch("number_of_gg_cells", &ptr_number_of_gg_cells);//, "number_of_gg_cells/I");
+    delayed_cells_tree->Branch("number_of_delayed_gg_cells", &ptr_number_of_delayed_gg_cells);//, "number_of_delayed_gg_cells/I");
+    delayed_cells_tree->Branch("number_of_not_delayed_gg_cells", &ptr_number_of_not_delayed_gg_cells);//, "number_of_not_delayed_gg_cells/I");
+    delayed_cells_tree->Branch("delayed_time_shift", &ptr_delayed_time_shift);//, "delayed_time_shift/I");
+    gg_cells_tree->Branch("hit_id_vector",     &ptr_hit_id);//, "hit_id/I");
+    gg_cells_tree->Branch("time_start_vector", &ptr_time_start);//, "time_start/D");
+    gg_cells_tree->Branch("x_start_vector",    &ptr_x_start);//, "x_start/D");
+    gg_cells_tree->Branch("y_start_vector",    &ptr_y_start);//, "y_start/D");
+    gg_cells_tree->Branch("z_start_vector",    &ptr_z_start);//, "z_start/D");
+    gg_cells_tree->Branch("x_stop_vector",     &ptr_x_stop);//, "x_stop/D");
+    gg_cells_tree->Branch("y_stop_vector",     &ptr_y_stop);//, "y_stop/D");
+    gg_cells_tree->Branch("z_stop_vector",     &ptr_z_stop);//, "z_stop/D");
+    gg_cells_tree->Branch("px_start_vector",   &ptr_px_start);//, "px_start/D");
+    gg_cells_tree->Branch("py_start_vector",   &ptr_py_start);//, "py_start/D");
+    gg_cells_tree->Branch("pz_start_vector",   &ptr_pz_start);//, "pz_start/D");
+    
+    static const Int_t MAXIMUM_DELAYED_TIME = 50;
+    
     // Event record :
     datatools::things ER;
     //std::ofstream file(producted_file.c_str());
@@ -144,7 +157,7 @@ int main(int  argc_ , char ** argv_)
 	    // Access to the "SD" bank with a stored `mctools::simulated_data' :
 	    const mctools::simulated_data & SD = ER.get<mctools::simulated_data>(SD_bank_label);
 	    ptr_hit_id->clear();
-	    //ptr_time_start->clear();
+	    ptr_time_start->clear();
 	    ptr_x_start-> clear();
 	    ptr_y_start->clear();
 	    ptr_z_start->clear();
@@ -154,79 +167,89 @@ int main(int  argc_ , char ** argv_)
 	    ptr_px_start->clear();
 	    ptr_py_start->clear();
 	    ptr_pz_start->clear();
+	    ptr_number_of_gg_cells->clear();
+	    ptr_number_of_delayed_gg_cells->clear();
+	    ptr_number_of_not_delayed_gg_cells->clear();
+	    ptr_delayed_time_shift->clear();  
 
-	    vect_time_start.clear();
+	    for (Int_t delayed_time_shift = 10; delayed_time_shift <= MAXIMUM_DELAYED_TIME; delayed_time_shift+= 10) // Time for delayed gg cells (in ns)
+	      {	 
+ 
+		std::clog << "Delayed time shift = " << delayed_time_shift << std::endl;
+		Int_t number_of_gg_cells = 0;
+		Int_t number_of_delayed_gg_cells = 0;
+		Int_t number_of_not_delayed_gg_cells = 0;
 
-	    if (SD.has_step_hits("gg"))
-	      {
-		SD.tree_dump(std::clog, "Simulated Data : ", "INFO : ");
-		mctools::simulated_data::hit_handle_collection_type BSHC = SD.get_step_hits("gg");
-		std::clog << "BSCH step hits # = " << BSHC.size() << std::endl;
-		number_of_gg_cells =  BSHC.size();
-		int count = 0;
-		for (mctools::simulated_data::hit_handle_collection_type::const_iterator i = BSHC.begin();
-		     i != BSHC.end();
-		     i++) 
+		if (SD.has_step_hits("gg"))
 		  {
-		    const mctools::base_step_hit & BSH = i->get();
-		    BSH.tree_dump(std::clog, "A Geiger Base Step Hit : ", "INFO : ");
-		    // std::string particle_name = BSH.get_particle_name();
+		    //SD.tree_dump(std::clog, "Simulated Data : ", "INFO : ");
+		    mctools::simulated_data::hit_handle_collection_type BSHC = SD.get_step_hits("gg");
+		    //std::clog << "BSCH step hits # = " << BSHC.size() << std::endl;
+		    number_of_gg_cells =  BSHC.size();
+		    int count = 0;
+		    for (mctools::simulated_data::hit_handle_collection_type::const_iterator i = BSHC.begin();
+			 i != BSHC.end();
+			 i++) 
+		      {
+			const mctools::base_step_hit & BSH = i->get();
+			//BSH.tree_dump(std::clog, "A Geiger Base Step Hit : ", "INFO : ");
+			// std::string particle_name = BSH.get_particle_name();
 
-		    Int_t hit_id = count;
-		    ptr_hit_id->push_back(hit_id);
+			Int_t hit_id = count;
+			Double_t time_start = BSH.get_time_start();
+			geomtools::vector_3d position_start_vector = BSH.get_position_start();
+			geomtools::vector_3d position_stop_vector  = BSH.get_position_stop();
+			geomtools::vector_3d momentum_start_vector = BSH.get_momentum_start();
+			Double_t x_start = position_start_vector.getX();
+			Double_t y_start = position_start_vector.getY();
+			Double_t z_start = position_start_vector.getZ();
+			Double_t x_stop  = position_stop_vector.getX();
+			Double_t y_stop  = position_stop_vector.getY();
+			Double_t z_stop  = position_stop_vector.getZ();
+			Double_t px_start = momentum_start_vector.getX();
+			Double_t py_start = momentum_start_vector.getY();
+			Double_t pz_start = momentum_start_vector.getZ();
 
-		    double time_start = BSH.get_time_start();
-		    //ptr_time_start->push_back(time_start);
-		    vect_time_start.push_back(time_start);
+			// Just to fill one time these informations
+			if (delayed_time_shift == 10)
+			  {
+			    ptr_hit_id->push_back(hit_id);
+			    ptr_time_start->push_back(time_start);
+			    ptr_x_start->push_back(x_start);
+			    ptr_y_start->push_back(y_start);
+			    ptr_z_start->push_back(z_start);
+			    ptr_x_stop->push_back(x_stop);
+			    ptr_y_stop->push_back(y_stop);
+			    ptr_z_stop->push_back(z_stop);
+			    ptr_px_start->push_back(px_start);
+			    ptr_py_start->push_back(py_start);
+			    ptr_pz_start->push_back(pz_start);
+			  }
 
-		    geomtools::vector_3d position_start_vector = BSH.get_position_start();
-		    geomtools::vector_3d position_stop_vector  = BSH.get_position_stop();
-		    geomtools::vector_3d momentum_start_vector = BSH.get_momentum_start();
-		    Double_t x_start = position_start_vector.getX();
-		    Double_t y_start = position_start_vector.getY();
-		    Double_t z_start = position_start_vector.getZ();
-		    Double_t x_stop  = position_stop_vector.getX();
-		    Double_t y_stop  = position_stop_vector.getY();
-		    Double_t z_stop  = position_stop_vector.getZ();
-		    Double_t px_start = momentum_start_vector.getX();
-		    Double_t py_start = momentum_start_vector.getY();
-		    Double_t pz_start = momentum_start_vector.getZ();
-
-		    ptr_x_start->push_back(x_start);
-		    ptr_y_start->push_back(y_start);
-		    ptr_z_start->push_back(z_start);
-		    ptr_x_stop->push_back(x_stop);
-		    ptr_y_stop->push_back(y_stop);
-		    ptr_z_stop->push_back(z_stop);
-		    ptr_px_start->push_back(px_start);
-		    ptr_py_start->push_back(py_start);
-		    ptr_pz_start->push_back(pz_start);
-
-
-		    if (time_start > 10) number_of_delayed_gg_cells++; // time in ns
-		    else{
-		      number_of_not_delayed_gg_cells++;
-		     }
-		    std::clog << " Count (aka hit id ) = " << count << std::endl;
-		    count++;
+			if (time_start > delayed_time_shift) number_of_delayed_gg_cells++; // time in ns
+			else number_of_not_delayed_gg_cells++;
+			
+			count++;
+		      }
 		  }
-		std::clog << "Vect time start size = " << vect_time_start.size() << std::endl;
-		tree->Fill();
+		std::clog << "GG Cells #" << number_of_gg_cells << " Delayed GG Cells #" << number_of_delayed_gg_cells << " Not Delayed GG Cells #" << number_of_not_delayed_gg_cells << std::endl;
+		ptr_delayed_time_shift->push_back(delayed_time_shift);
+		ptr_number_of_gg_cells->push_back(number_of_gg_cells);
+	        ptr_number_of_delayed_gg_cells->push_back(number_of_delayed_gg_cells);
+	        ptr_number_of_not_delayed_gg_cells->push_back(number_of_not_delayed_gg_cells);
 	      }
 	  }
 
-	number_of_delayed_gg_cells = 0;
-	number_of_gg_cells = 0;
-	number_of_not_delayed_gg_cells = 0;
-
+	gg_cells_tree->Fill();	
+	delayed_cells_tree->Fill();
+	
 	ER.clear();
 	psd_count++;
-
+	
 	//std::clog << "DEBUG : psd count " << psd_count << std::endl;
 	DT_LOG_NOTICE(logging, "Simulated data #" << psd_count);
       }
     
-
     root_file->Write();
     root_file->Close();
 
