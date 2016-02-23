51da3b58bac1439c1e50ab07a686efd9bce60a38
Guillaume Oliviero <goliviero@lpccaen.in2p3.fr>
2016-02-23 11:48:04 +0000

    Add a flag for geiger cells already hit

--- modules/Digitization/source/falaise/snemo/digitization/sd_to_geiger_signal_algo.cc
+++ modules/Digitization/source/falaise/snemo/digitization/sd_to_geiger_signal_algo.cc
@@ -102,58 +102,88 @@ namespace snemo {
       int error_code = EXIT_SUCCESS;
       datatools::logger::priority logging = datatools::logger::PRIO_FATAL;
       try { 
-	//std::clog << "DEBUG : BEGINING OF SD TO GEIGER SIGNAL PROCESS " << std::endl;
-	//std::clog << "**************************************************************" << std::endl;
 	// pickup the ID mapping from the geometry manager:
 	const geomtools::mapping & the_mapping = _geo_manager_->get_mapping();
 
 	// Loop on Geiger step hits:
 	const size_t number_of_hits = sd_.get_number_of_step_hits("gg");
 	
+	// New sd bank
+	mctools::simulated_data flaged_sd = sd_;
+	
+	// We have to flag the gg cells already hit before (maybe take into account the dead time of a GG cell)
+	for (size_t ihit = 0; ihit < number_of_hits; ihit++)
+	  {
+	    mctools::base_step_hit & geiger_hit = flaged_sd.grab_step_hit("gg", ihit);	    
+	    for (size_t jhit = ihit + 1; jhit < number_of_hits; jhit++)
+	      {
+		mctools::base_step_hit & other_geiger_hit = flaged_sd.grab_step_hit("gg", jhit);
+		if (geiger_hit.get_geom_id() == other_geiger_hit.get_geom_id())
+		  {
+		    const double gg_hit_time       = geiger_hit.get_time_start();
+		    const double other_gg_hit_time = other_geiger_hit.get_time_start();
+		    if (gg_hit_time > other_gg_hit_time) 
+		      {
+			bool already_hit = true;
+			geiger_hit.grab_auxiliaries().store("already_hit", already_hit);
+		      }
+		    else 
+		      {
+			bool already_hit = true;
+			other_geiger_hit.grab_auxiliaries().store("already_hit", already_hit);
+		      }
+		  }
+	      }
+	  }
+
 	int32_t geiger_signal_hit_id = 0;
 	
 	for (size_t ihit = 0; ihit < number_of_hits; ihit++)
 	  {
-	    const mctools::base_step_hit & geiger_hit = sd_.get_step_hit("gg", ihit);
-
-	    // extract the corresponding geom ID:
-	    const geomtools::geom_id & geiger_gid = geiger_hit.get_geom_id();
-
-	    // extract the geom info of the corresponding cell:
-	    const geomtools::geom_info & ginfo = the_mapping.get_geom_info(geiger_gid);
-
-	    // the position of the ion/electron pair creation within the cell volume:
-	    const geomtools::vector_3d & ionization_world_pos = geiger_hit.get_position_start();
-
-	    // the position of the Geiger avalanche impact on the anode wire:
-	    const geomtools::vector_3d & avalanche_impact_world_pos = geiger_hit.get_position_stop();
-
-	    // compute the position of the anode impact in the drift cell coordinates reference frame:
-	    geomtools::vector_3d avalanche_impact_cell_pos;
-	    ginfo.get_world_placement().mother_to_child(avalanche_impact_world_pos,
-							avalanche_impact_cell_pos);
-	    // longitudinal position:
-	    const double longitudinal_position = avalanche_impact_cell_pos.z();
-
-	    // true drift distance:
-	    const double drift_distance =(avalanche_impact_world_pos - ionization_world_pos).mag();
-
-	    // the time of the ion/electron pair creation:
-	    const double ionization_time = geiger_hit.get_time_start();
-
-	    /*** Anode TDC ***/
-	    // randomize the expected Geiger drift time:
-	    const double expected_drift_time = _anode_drift_time_calculation(drift_distance);
-
-	    const double anode_time          = ionization_time + expected_drift_time;
+	    const mctools::base_step_hit & geiger_hit = flaged_sd.get_step_hit("gg", ihit);
 	    
-	    geiger_signal & gg_signal = signal_data.add_geiger_signal();
-	    gg_signal.set_header(geiger_signal_hit_id,
-				 geiger_gid);
-	    gg_signal.set_data(anode_time);
-	    gg_signal.grab_auxiliaries().store("hit.id", geiger_signal_hit_id);
-	    //gg_signal.tree_dump(std::clog, "***** Geiger Signal : *****", "INFO : ");
-	    geiger_signal_hit_id++;
+	    if (geiger_hit.get_auxiliaries().has_flag("already_hit")) {}
+	    else
+	      {
+		// extract the corresponding geom ID:
+		const geomtools::geom_id & geiger_gid = geiger_hit.get_geom_id();
+
+		// extract the geom info of the corresponding cell:
+		const geomtools::geom_info & ginfo = the_mapping.get_geom_info(geiger_gid);
+
+		// the position of the ion/electron pair creation within the cell volume:
+		const geomtools::vector_3d & ionization_world_pos = geiger_hit.get_position_start();
+
+		// the position of the Geiger avalanche impact on the anode wire:
+		const geomtools::vector_3d & avalanche_impact_world_pos = geiger_hit.get_position_stop();
+
+		// compute the position of the anode impact in the drift cell coordinates reference frame:
+		geomtools::vector_3d avalanche_impact_cell_pos;
+		ginfo.get_world_placement().mother_to_child(avalanche_impact_world_pos,
+							    avalanche_impact_cell_pos);
+		// longitudinal position:
+		const double longitudinal_position = avalanche_impact_cell_pos.z();
+
+		// true drift distance:
+		const double drift_distance =(avalanche_impact_world_pos - ionization_world_pos).mag();
+
+		// the time of the ion/electron pair creation:
+		const double ionization_time = geiger_hit.get_time_start();
+
+		/*** Anode TDC ***/
+		// randomize the expected Geiger drift time:
+		const double expected_drift_time = _anode_drift_time_calculation(drift_distance);
+
+		const double anode_time          = ionization_time + expected_drift_time;
+	    
+		geiger_signal & gg_signal = signal_data.add_geiger_signal();
+		gg_signal.set_header(geiger_signal_hit_id,
+				     geiger_gid);
+		gg_signal.set_data(anode_time);
+		gg_signal.grab_auxiliaries().store("hit.id", geiger_signal_hit_id);
+		//gg_signal.tree_dump(std::clog, "***** Geiger Signal : *****", "INFO : ");
+		geiger_signal_hit_id++;
+	      }
 	  }	  
 	
       }
