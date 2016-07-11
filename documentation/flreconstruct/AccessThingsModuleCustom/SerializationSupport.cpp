9bd84ca4fd2b3c589cdb50e6f3a036062059f9f3
Francois Mauger <mauger@lpccaen.in2p3.fr>
2016-07-11 16:22:53 +0000

    some comments

--- documentation/flreconstruct/AccessThingsModuleCustom/SerializationSupport.cpp
+++ documentation/flreconstruct/AccessThingsModuleCustom/SerializationSupport.cpp
@@ -6,6 +6,7 @@
 #include "MyDataType-serial.ipp"
 
 // - Bayeux/datatools:
+// Note: maybe the pragmas could be removed...
 #ifdef __GNUC__
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wshadow"
@@ -22,7 +23,8 @@ DATATOOLS_SERIALIZATION_CLASS_SERIALIZE_INSTANTIATE_ALL(MyDataType)
 // 2016-07-11 FM: it seems that if I remove this macro above, the system
 // still works as long as the 'bayeux/datatools/archives_instantiation.h'
 // is included. AFAIR, it was not the case with former versions of the
-// Boost/library.
+// Boost/library. I let the macro here because I'm not sure yet of possible
+// side-effects if it is removed.
 
 // Boost/Serialization class registration implementation:
 BOOST_CLASS_EXPORT_IMPLEMENT(MyDataType)
