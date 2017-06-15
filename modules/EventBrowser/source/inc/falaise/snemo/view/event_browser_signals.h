a04a598bd9800c85d19dc25ab5883eb27107332f
Xavier Garrido <xavier.garrido@gmail.com>
2017-06-15 08:14:13 +0200

    ressurect complex selection

--- modules/EventBrowser/source/inc/falaise/snemo/view/event_browser_signals.h
+++ modules/EventBrowser/source/inc/falaise/snemo/view/event_browser_signals.h
@@ -99,9 +99,9 @@ enum button_signals_type
     AND_SELECTION,
     XOR_SELECTION,
     ENABLE_LOGIC_SELECTION,
-    ENABLE_COMPLEX_SELECTION,
     ENABLE_EH_SELECTION,
     ENABLE_SD_SELECTION,
+    ENABLE_COMPLEX_SELECTION,
 
     // Signals for event navigation control
     STATUS_BAR,
