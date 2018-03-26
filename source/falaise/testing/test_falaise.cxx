fbab47c34f15db3d047e46b571fb7a78a4c55254
Francois Mauger <mauger@lpccaen.in2p3.fr>
2018-03-27 00:44:22 +0200

    Add check and print in test program

--- source/falaise/testing/test_falaise.cxx
+++ source/falaise/testing/test_falaise.cxx
@@ -6,10 +6,18 @@
 #include <iostream>
 #include <string>
 
+// Third party:
+// - Bayeux:
+#include <bayeux/datatools/kernel.h>
+#include <bayeux/datatools/library_info.h>
+#include <bayeux/datatools/urn_query_service.h>
+#include <bayeux/datatools/urn_db_service.h>
+
 // This project:
 #include <falaise/falaise.h>
 #include <falaise/resource.h>
 #include <falaise/version.h>
+#include <falaise/detail/falaise_sys.h>
 
 int main(int /* argc_ */, char** /* argv_ */) {
   falaise::initialize();
@@ -17,10 +25,27 @@ int main(int /* argc_ */, char** /* argv_ */) {
   try {
     std::clog << "Test program for the 'Falaise' library." << std::endl;
 
-    std::clog << "Falaise version : " << falaise::version::get_version() << std::endl;
-    std::clog << "Falaise resource dir : '" << falaise::get_resource_dir() << "'" << std::endl;
+    std::clog << "Falaise version       : " << falaise::version::get_version() << std::endl;
+    std::clog << "Falaise resource dir  : '" << falaise::get_resource_dir() << "'" << std::endl;
     std::clog << "Falaise resource file : '" << falaise::get_resource("README.rst") << "'"
               << std::endl;
+
+    {
+      if (datatools::kernel::is_instantiated()) {
+        datatools::kernel & krnl = datatools::kernel::instance();
+        if (krnl.has_library_info_register()) {
+          const datatools::library_info & lib_info_reg = krnl.get_library_info_register();
+          lib_info_reg.tree_dump(std::clog, "Kernel library info register: ");
+          std::clog << std::endl;
+        }
+        if (krnl.get_urn_query().has_db(falaise::detail::falaise_sys::fl_setup_db_name())) {
+          const datatools::urn_db_service & flUrnDb =
+            krnl.get_urn_query().get_db(falaise::detail::falaise_sys::fl_setup_db_name());
+          flUrnDb.tree_dump(std::clog, "Falaise library's URN Database Service: ");
+          std::clog << std::endl;
+        }
+      }
+    }
 
     std::clog << "The end." << std::endl;
   } catch (std::exception& x) {
