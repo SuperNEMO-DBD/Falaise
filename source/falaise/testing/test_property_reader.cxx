b0b57112fb5001815bd1e2f22404be2f5c498b37
Xavier Garrido <xavier.garrido@gmail.com>
2018-06-08 17:22:20 +0200

    Always catch exception by reference
    https://blog.knatten.org/2010/04/02/always-catch-exceptions-by-reference/

--- source/falaise/testing/test_property_reader.cxx
+++ source/falaise/testing/test_property_reader.cxx
@@ -14,7 +14,7 @@ TEST_CASE("property_reader scalars Work", "") {
 
   SECTION("Integer extraction") {
     REQUIRE(flp::getRequiredValue<int>(test, "foo") == 1);
-    REQUIRE_THROWS_AS(flp::getRequiredValue<double>(test, "foo"), flp::WrongType);
+    REQUIRE_THROWS_AS(flp::getRequiredValue<double>(test, "foo"), flp::WrongType&);
 
     REQUIRE(flp::getValueOrDefault<int>(test, "foo", 42) == 1);
     REQUIRE(flp::getValueOrDefault<int>(test, "ICouldBePresentButAmNot", 42) == 42);
@@ -22,13 +22,13 @@ TEST_CASE("property_reader scalars Work", "") {
 
   SECTION("Real extraction") {
     REQUIRE(flp::getRequiredValue<double>(test, "bar") == Approx(3.14));
-    REQUIRE_THROWS_AS(flp::getRequiredValue<int>(test, "bar"), flp::WrongType);
+    REQUIRE_THROWS_AS(flp::getRequiredValue<int>(test, "bar"), flp::WrongType&);
   }
 
   SECTION("Boolean extraction") {
     REQUIRE(flp::getRequiredValue<bool>(test, "baz"));
     REQUIRE_FALSE(flp::getValueOrDefault<bool>(test, "ICouldBePresentButAmNot", false));
-    REQUIRE_THROWS_AS(flp::getRequiredValue<bool>(test, "foo"), flp::WrongType);
+    REQUIRE_THROWS_AS(flp::getRequiredValue<bool>(test, "foo"), flp::WrongType&);
   }
 
   SECTION("String extraction") {
@@ -40,7 +40,7 @@ TEST_CASE("property_reader scalars Work", "") {
   SECTION("Failure conditions") {
     // Unsupported types should be a compile-time error
     REQUIRE_THROWS_AS(flp::getRequiredValue<double>(test, "IMustBePresentButAmNot"),
-                      flp::MissingKey);
+                      flp::MissingKey&);
   }
 }
 
@@ -68,7 +68,7 @@ TEST_CASE("property_reader vector extraction", "") {
     REQUIRE_NOTHROW(flp::getRequiredValue<std::vector<int> >(test, "singular"));
 
     // ... and is not convertible to a single element
-    REQUIRE_THROWS_AS(flp::getRequiredValue<int>(test, "singular"), flp::WrongType);
+    REQUIRE_THROWS_AS(flp::getRequiredValue<int>(test, "singular"), flp::WrongType&);
 
     // Defaults should work in place
     // NB: test composition does outside macro due to seeming limitation of not
