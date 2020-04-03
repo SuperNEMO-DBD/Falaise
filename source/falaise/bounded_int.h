9f5530a7e4e84375dd522f2b60b395b8561faffb
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2020-04-03 16:46:05 +0100

    Minimal bounded integer implementation and test
    The geometry and reconstruction algorithms in Falaise have many places
    where a limited range of integers is used to address "things", for
    example tracker cell indices. Having values outside these ranges is
    an error, and whilst these are tracked, the resulting checks are
    bolierplated throughout the code.
    
    Provide a basic "bounded_integer" type to represent a range
    [lower,upper] of integral values. Constructing or assigning with a
    value outside of this range results in an out of range exception.
    
    Implement minimal unit tests of this behaviour

---
+++ source/falaise/bounded_int.h
@@ -0,0 +1,56 @@
+// Copyright (c) 2020 by Ben Morgan <Ben.Morgan@warwick.ac.uk>
+// Copyright (c) 2020 by The University of Warwick
+//
+// This file is part of Falaise.
+//
+// Falaise is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Falaise is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Falaise.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef FALAISE_BOUNDED_INT_H
+#define FALAISE_BOUNDED_INT_H
+
+#include <cstdint>
+#include <exception>
+
+namespace falaise {
+
+template <int32_t lower_bound, int32_t upper_bound>
+class bounded_int {
+ public:
+  bounded_int() = default;
+  bounded_int(int32_t x) : value_{x} {
+    if (x < lower_bound || x > upper_bound) {
+      throw std::out_of_range("supplied value is out of range");
+    }
+  }
+
+  ~bounded_int() = default;
+
+  bounded_int(const bounded_int&) = default;
+  bounded_int(bounded_int&& rhs) = default;
+
+  bounded_int& operator=(const bounded_int&) = default;
+  bounded_int& operator=(bounded_int&&) = default;
+
+  operator int32_t() const { return value_; }
+
+  constexpr int32_t min() const { return lower_bound; }
+
+  constexpr int32_t max() const { return upper_bound; }
+
+ private:
+  int32_t value_ = lower_bound;
+};
+}  // namespace falaise
+
+#endif  // FALAISE_BOUNDED_INT_H
---
+++ source/falaise/test/test_bounded_int.cxx
@@ -0,0 +1,40 @@
+#include "catch.hpp"
+
+#include "falaise/bounded_int.h"
+
+TEST_CASE("Raw bounded construction", "") {
+  falaise::bounded_int<-1,42> x{};
+  REQUIRE(x == -1);
+  REQUIRE(x.min() == -1);
+  REQUIRE(x.max() == 42);
+}
+
+TEST_CASE("Out of bounds construction", "") {
+  using address = falaise::bounded_int<0,119>;
+
+  // Must be able to construct at bounds
+  REQUIRE_NOTHROW(address good{0});
+  REQUIRE_NOTHROW(address good{119});
+
+  // Out of bounds must throw
+  REQUIRE_THROWS_AS(address bad{-1}, std::out_of_range);
+  REQUIRE_THROWS_AS(address bad{120}, std::out_of_range);
+}
+
+TEST_CASE("Assignment operators work", "") {
+  using address = falaise::bounded_int<-42,119>;
+
+  address x;
+
+  // Assignment must work
+  x = 3;
+  REQUIRE(x == 3);
+
+  // Assignment on bounds must work
+  REQUIRE_NOTHROW(x = -42);
+  REQUIRE_NOTHROW(x = 119);
+
+  // Out of bounds assignment must throw
+  REQUIRE_THROWS_AS(x = -43, std::out_of_range);
+  REQUIRE_THROWS_AS(x = -120, std::out_of_range);
+}
