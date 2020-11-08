4d995b7ace85199065bf8ddfdcd5bb0e051438da
Paolo Franchini <pfranchi@cca004.in2p3.fr>
2020-11-08 01:33:36 +0100

    Implemented comments

--- source/falaise/snemo/services/dead_cells.cc
+++ source/falaise/snemo/services/dead_cells.cc
@@ -29,13 +29,25 @@ namespace snemo {
   int dead_cells_svc::LoadCells(std::string deadcells_filename) {
     // Creates a vector of _ONLY_ Bad cells (e.g reading from a DB or from a file like now);
     // cells not included in the vector are considered to be Good;
-    // the vector here created can be independently accessed
+    // the vector here created can be also accessed (size, loops).
+
     try {
       std::ifstream infile(deadcells_filename);
       int side, layer, column, status;
+      // I assume here that the `status` is saved as `int`, therefore the switch
+      
       while (infile >> side >> layer >> column >> status){
+	cell_status cs = cell_status::good;
+	switch(status) {
+	case 1: cs = cell_status::dead; break;
+	case 2: cs = cell_status::cathode_ground_top; break;
+	case 3: cs = cell_status::cathode_ground_bottom; break;
+	case 4: cs = cell_status::cathode_ground_both; break;
+	case 5: cs = cell_status::cathode_cathode; break;
+	case 6: cs = cell_status::other; break;
+	}  
 
-	cell_id c(side, layer, column, status);
+	cell_id c(side, layer, column, cs);
 	cells.push_back(c);
 
       }
@@ -48,7 +60,7 @@ namespace snemo {
     return 0;
   }
 
-  int dead_cells_svc::CellStatus(cell_id cell, int run_number) {
+  cell_status dead_cells_svc::CellStatus(cell_id cell, int run_number) {
     
     if ( run_number != 0 ) // left like this to be used, since will be a DB IOV thing
       // search for the specific cell in the vector
@@ -56,19 +68,19 @@ namespace snemo {
 	if ( (c.GetSide()==cell.GetSide()) && (c.GetLayer()==cell.GetLayer()) && (c.GetColumn()==cell.GetColumn()) )
 	  return c.GetStatus(); // Bad cell, present in the vector
       }
-        
-    return status::good; // Good cell, because not present in the vector
+
+    return cell_status::good; // Good cell, because not present in the vector
 
   } // int dead_cells_svc::CellStatus
   
-  int dead_cells_svc::CellStatus(int side, int layer, int column, int run_number) {
+  cell_status dead_cells_svc::CellStatus(int side, int layer, int column, int run_number) {
     cell_id cell(side,layer,column);
     return dead_cells_svc::CellStatus(cell, run_number);
   }
   
   bool dead_cells_svc::isABadCell(cell_id cell, int run_number) {
     
-    if (CellStatus(cell, run_number) != status::good)
+    if (CellStatus(cell, run_number) != cell_status::good)
       return 1;
     else
       return 0;
@@ -91,7 +103,7 @@ namespace snemo {
   
   bool dead_cells_svc::isADeadCell(cell_id cell, int run_number) {
 
-    if (CellStatus(cell, run_number)== status::dead)
+    if (CellStatus(cell, run_number)== cell_status::dead)
       return 1;
     else
       return 0;
--- source/falaise/snemo/services/dead_cells.h
+++ source/falaise/snemo/services/dead_cells.h
@@ -1,16 +1,5 @@
 //! \file falaise/snemo/services/dead_cells.h
 //! \brief Tracker Dead Cells service in SuperNEMO
-//
-// more doxygen to come...
-//
-// status  description
-// ---------------------------------------------------------------------------
-// 0       good working cell            isAGoodCell==true
-// 1       dead cells with dead anode   isADeadCell==true and isABadCell==true
-// 2       cathode-to-ground short      isABadCell==true
-// 3       cathode-to-cathode short     isABadCell==true
-// 4       other                        isABadCell==true
-
 
 #ifndef SNEMO_DEAD_CELLS_SVC_HH
 #define SNEMO_DEAD_CELLS_SVC_HH
@@ -25,28 +14,30 @@
 namespace snemo {
 
   // data member for the possible statuses of a cell  
-  enum status {
-    good = 0,            // good working cell
-    dead = 1,            // dead cells with dead anode
-    cathode_ground = 2,  // cathode-to-ground short
-    cathode_cathode = 3, // cathode-to-cathode short
-    other = 4            // other
+  enum class cell_status {
+    good = 0,                   // good working
+    dead = 1,                   // dead cells with dead anode |
+    cathode_ground_top = 2,     // cathode-to-ground short    |
+    cathode_ground_bottom = 3,  // cathode-to-ground short    | bad
+    cathode_ground_both = 4,    // cathode-to-ground short    |
+    cathode_cathode = 5,        // cathode-to-cathode short   |
+    other = 6                   // other                      |
   };
 
   // data member defining a cell structure
   class cell_id {
   public:
-    using side_id = falaise::bounded_int<0,1>;
-    using layer_id = falaise::bounded_int<0,8>;
-    using column_id = falaise::bounded_int<0,112>;
-    using status_id = falaise::bounded_int<0,4>;
 
+    using side_id = falaise::bounded_int<0,1>;                                                                                                                      
+    using layer_id = falaise::bounded_int<0,8>;  
+    using column_id = falaise::bounded_int<0,112>;
+    
     // constructors
     cell_id(side_id s, layer_id l, column_id c){  // use this to search for the status of a cell
       SetCell(s,l,c);
     }
 
-    cell_id(side_id s, layer_id l, column_id c, status_id st){ // use this to create a cell knowing everything
+    cell_id(side_id s, layer_id l, column_id c, cell_status st){ // use this to create a cell knowing everything
       SetCell(s,l,c,st);
     }
     
@@ -57,7 +48,7 @@ namespace snemo {
       column_ = c;
     }
 
-    void SetCell(side_id s, layer_id l, column_id c, status_id st){  // use this to create a cell knowing everything
+    void SetCell(side_id s, layer_id l, column_id c, cell_status st){  // use this to create a cell knowing everything
       side_ = s;
       layer_ = l;
       column_ = c;
@@ -77,7 +68,7 @@ namespace snemo {
       return column_;
     }
 
-    int GetStatus(){
+    cell_status GetStatus(){
       return status_;
     }
 
@@ -85,15 +76,12 @@ namespace snemo {
     side_id side_;
     layer_id layer_;
     column_id column_;
-    status_id status_;
+    cell_status status_;
   };
   
   class dead_cells_svc : public datatools::base_service {
   public:
 
-    // Vector with list of Bad cells to be filled by the function LoadCells()
-    std::vector<cell_id> cells;    
-    
     // Inherited pure virtual member functions that must be overridden
     int initialize(const datatools::properties&, datatools::service_dict_type&) override;
     int reset() override;
@@ -103,16 +91,24 @@ namespace snemo {
     // have left the (side,layer,column) parameters option just for convenience
     int LoadCells(std::string deadcells_filename);
 
-    int CellStatus(cell_id cell, int run_number);
-    int CellStatus(int side, int layer, int column, int run_number);
+    cell_status CellStatus(cell_id cell, int run_number);
+    cell_status CellStatus(int side, int layer, int column, int run_number);
     bool isABadCell(cell_id cell, int run_number);
     bool isABadCell(int side, int layer, int column, int run_number);
     bool isAGoodCell(cell_id cell, int run_number);
     bool isAGoodCell(int side, int layer, int column, int run_number);
     bool isADeadCell(cell_id cell, int run_number);
     bool isADeadCell(int side, int layer, int column, int run_number);
+
+    size_t size() const { return cells.size();}
+    std::vector<cell_id>::const_iterator begin() const { return cells.begin();}
+    std::vector<cell_id>::const_iterator end() const { return cells.end();}
     
   private:
+
+    // Vector with list of Bad cells to be filled by the function LoadCells()
+    std::vector<cell_id> cells;    
+
     DATATOOLS_SERVICE_REGISTRATION_INTERFACE(dead_cells_svc)
       };
   
@@ -123,6 +119,7 @@ namespace snemo {
     using instance_type = service_type;
     static instance_type* get(service_type& sm) { return &sm; }
   };
+
 }  // namespace snemo
 
 #endif  // SNEMO_DEAD_CELLS_SVC_HH
